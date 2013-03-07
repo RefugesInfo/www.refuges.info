551d49aa978aa30f046cde6c2fc4583db0bf28df
OpenSourceWay <opensourceway@laposte.net>
2013-03-07 19:15:44 +0100

    Premiere mise en place des repertoire version mobile

---
+++ mobile/index.php
@@ -0,0 +1,58 @@
+<?php 
+/*******************************************************************************
+Ecran d'accueil de la version mobile
+
+Contient le code PHP de la page
+Le code html est dans /vues/*.html
+Le code javascript est dans /vues/*.js
+Les variables sont passées dans l'objet $modele->...
+*******************************************************************************/
+
+require_once ('../modeles/config.php');
+require_once ("fonctions_nouvelles.php");
+require_once ("fonctions_polygones.php");
+require_once ("fonctions_autoconnexion.php");
+
+$modele = new stdClass;
+$modele->titre = 'Carte de refuges, cabanes et abris de montagne | Version mobile';
+$modele->java_lib [] = 'http://maps.google.com/maps/api/js?v=3&amp;sensor=false';
+$modele->java_lib [] = $config['chemin_openlayers'].'OpenLayers.js';
+
+$conditions_notre_zone = new stdClass;
+$conditions_notre_zone->ids_polygones=$config['id_zone_accueil'];
+$polygones=infos_polygones($conditions_notre_zone);
+$modele->bbox=$polygones[0]->bbox;
+
+// liens vers les zones
+$conditions = new stdClass;
+$conditions->ids_polygone_type=$config['id_zone'];
+$zones=infos_polygones($conditions);
+// Ajoute les liens vers les autres zones
+if ($zones)
+  foreach ($zones as $zone) // FIXME ce foreach et preg_replace sont complètement ridicule mais sautero avec fusion massif/nav
+    $modele->zones [$zone->nom_polygone] = lien_polygone($zone)."?massifs";
+
+// Réinitialise les paramètres de réaffichage des pages suivantes, notamment la couche par défaut = Google
+setcookie ('Olparams', '', time() - 3600, '/');
+
+// News
+$modele->commentaires = $commentaires;
+$modele->stat = stat_site ();
+$modele->general = $general;
+$modele->quoi = $_GET ['quoi']
+			  ? $_GET ['quoi']
+			  : 'commentaires,points,forums';
+			  
+// Préparation de la liste des photos récentes
+$conditions = new stdclass();
+$conditions->limite=5;
+$conditions->avec_photo=True;
+$conditions->avec_infos_point=True;
+$modele->photos_recentes=infos_commentaires($conditions);
+
+// On affiche le tout
+$modele->type = 'index';
+include ($config['chemin_vues']."_entete.html");
+include ($config['chemin_vues']."$modele->type.html");
+include ($config['chemin_vues']."_pied.html");
+?>
---
+++ mobile/js/index.js
@@ -0,0 +1,46 @@
+<?// Script lié à la page d'acceuil
+
+// Ce fichier ne doit contenir que du code javascript destiné à être inclus dans la page
+// $modele contient les données passées par le fichier PHP
+// $config les données communes à tout WRI
+
+// 17/10/11 Dominique : Création
+// 23/10/11 Dominique : Retour ici du code spécifique à la page qui avait été mis dans la bibliothèque
+// 15/04/11 Dominique : Passage en OL2.11
+// 08/05/12 Dominique : Retour en templates simples
+// 12/07/12 Dominique : Enrichissement avec les news
+// 15/02/13 jmb : gestion des zones
+?>
+
+// Crée la carte dés que la page est chargée
+window.onload = function () {
+	var map = new OpenLayers.Map ('accueil', {
+		displayProjection: new OpenLayers.Projection ('EPSG:4326'), // Données en °
+		controls: [
+			new OpenLayers.Control.Navigation(),
+			new OpenLayers.Control.Attribution()
+		],
+		layers: [
+			new OpenLayers.Layer.Google ('Google', {type: google.maps.MapTypeId.TERRAIN})
+		]
+	});
+	
+	// Positionne la carte sur la zone donnée par le .PHP
+	var bornes = new OpenLayers.Bounds ( <?=$modele->bbox?> ) 
+				.transform (map.displayProjection, map.getProjectionObject());
+
+	map.setCenter (
+		bornes.getCenterLonLat (), 
+		map.getZoomForExtent (bornes)
+	);
+	
+	// Ajoute les couches vectorielles avec controle
+	map.addLayers ([
+		new OpenLayers.Layer.GMLSLD ('Massifs', {	
+			urlGML: '/exportations/massifs-gml.php',
+			projection: 'EPSG:4326', // Le GML est fourni en degminsec
+			urlSLD: OpenLayers._getScriptLocation() + 'refuges-info-sld.xml',
+			styleName: 'Massifs'
+		})
+	]);
+}
