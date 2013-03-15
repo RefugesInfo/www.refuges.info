260cc7deb8345d1ba4a0c49942563ee8311f85fe
OpenSourceWay <opensourceway@laposte.net>
2013-03-15 22:27:04 +0100

    Ajout d'une version de carte avec AJAX/GeoLSON qui marche. TODO: récupérer le code de mise en style de l'ancienne version.

--- mobile/js/index.js
+++ mobile/js/index.js
@@ -6,12 +6,10 @@
 
 // 14/03/2013 Léo - Création
 
-
+/*
 
 // On actualise les points
 function actualiseGeoJSON() {
-
-
 	// On créé un style que jes points prennent par défaut
 	var defaultIcon = L.icon({
 		iconUrl: '../images/icones/cabane.png',
@@ -80,3 +78,82 @@ map.locate({setView: true, maxZoom: 14});
 map.on('locationerror', onLocationError);
 map.on('locationfound', onLocationFound);
 map.on('moveend', onMove); // Si l'utilisateur a fini de trifouiller la position de la carte, on actualise les points
+
+*/
+
+
+
+var map;
+var ajaxRequest;
+var plotlist;
+var plotlayers=[];
+
+function initmap(){
+	// set up the AJAX stuff
+	ajaxRequest=GetXmlHttpObject();
+	if (ajaxRequest==null) {
+		alert ("This browser does not support HTTP Request");
+		return;
+	}
+
+	// set up the map
+	map = new L.Map('map');
+
+	var MRIUrl='http://maps.refuges.info/hiking/{z}/{x}/{y}.png';
+	var MRIAttrib='&copy; Contributeurs d\'<a href="http://openstreetmap.org">OpenStreetMap</a>';
+	var MRI = new L.TileLayer(MRIUrl, {minZoom: 1, maxZoom: 12, attribution: MRIAttrib});		
+
+	map.setView(new L.LatLng(45.9, 7.7),9);
+	map.attributionControl.setPrefix('');
+	map.addLayer(MRI);
+	askForPlots();
+	map.on('moveend', onMapMove);
+}
+
+function askForPlots() {
+	// request the marker info with AJAX for the current bounds
+	var bounds=map.getBounds();
+	var minll=bounds.getSouthWest();
+	var maxll=bounds.getNorthEast();
+	var msg='../exportations/exportations.php?format=geojson&bbox='+minll.lng+','+minll.lat+','+maxll.lng+','+maxll.lat;
+	ajaxRequest.onreadystatechange = stateChanged; 
+	ajaxRequest.open('GET', msg, true);
+	ajaxRequest.send(null); 
+}
+
+function onMapMove(e) { askForPlots(); }
+
+function GetXmlHttpObject() {
+	if (window.XMLHttpRequest) { return new XMLHttpRequest(); }
+	if (window.ActiveXObject)  { return new ActiveXObject("Microsoft.XMLHTTP"); }
+	return null;
+}
+
+function stateChanged() {
+	// if AJAX returned a list of markers, add them to the map
+	if (ajaxRequest.readyState==4) {
+		//use the info here that was returned
+		if (ajaxRequest.status==200) {
+			plotlist=eval("(" + ajaxRequest.responseText + ")");
+			removeMarkers();
+			for (i=0;i<plotlist.features.length;i++) {
+				var plotll = new L.LatLng(plotlist.features[i].geometry.coordinates[1],plotlist.features[i].geometry.coordinates[0]);
+				plotll.lat=""+plotll.lat;
+				plotll.lng=""+plotll.lng;
+				var plotmark = new L.Marker(plotll);
+				plotmark.data=plotlist.features[i];
+				map.addLayer(plotmark);
+				plotmark.bindPopup('<a href="' + plotlist.features[i].properties.url + '">' + plotlist.features[i].properties.nom + '</a>');
+				plotlayers.push(plotmark);
+			}
+		}
+	}
+}
+
+function removeMarkers() {
+	for (i=0;i<plotlayers.length;i++) {
+		map.removeLayer(plotlayers[i]);
+	}
+	plotlayers=[];
+}
+
