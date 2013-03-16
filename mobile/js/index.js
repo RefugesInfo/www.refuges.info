da529dc3e49183f03dd1f0549aa71a3258c8c785
OpenSourceWay <opensourceway@laposte.net>
2013-03-16 15:51:46 +0100

    Carte fonctionnelle : Sans leaflet.ajax, avec géolocalisation, avec styles pour les points

--- mobile/js/index.js
+++ mobile/js/index.js
@@ -1,4 +1,4 @@
-// Script lié à la page d'acceuil
+// Script lié à la page d'accueil
 
 // Ce fichier ne doit contenir que du code javascript destiné à être inclus dans la page
 // $modele contient les données passées par le fichier PHP
@@ -6,150 +6,106 @@
 
 // 14/03/2013 Léo - Création
 
-/*
+/************************* DÉBUT FICHIER *************************/
 
-// On actualise les points
-function actualiseGeoJSON() {
-	// On créé un style que jes points prennent par défaut
-	var defaultIcon = L.icon({
-		iconUrl: '../images/icones/cabane.png',
-		iconSize: [16, 16],
-		iconAnchor: [8, 8],
-		popupAnchor: [0, -8]
-	});
-
-	// Pour chaque point, on le créé avec style et popup
-	function onEachFeature(feature, layer) {
-		var popupContent = '<a href="' + feature.properties.url + '">' + feature.properties.nom + "</a>";
-		layer.bindPopup(popupContent);
-		var cabaneIcon = L.icon({
-			iconUrl: '../images/icones/' + feature.properties.type + '.png',
-			iconSize: [16, 16],
-			iconAnchor: [8, 8],
-			popupAnchor: [0, -8]
-		});
-		layer.setIcon(cabaneIcon);
-	}
-
-	// Appel des points
-	var GeoJSONlayer = L.geoJson.ajax('../exportations/exportations.php?format=geojson&bbox=' + map.getBounds().toBBoxString() + '',{
-		onEachFeature: onEachFeature,
-
-		pointToLayer: function (feature, latlng) {
-			return L.marker(latlng, {icon: defaultIcon});
-		}
-	}).addTo(map);
-
-
-	// if (GeoJSONlayer != undefined)
- //    {
- //        map.removeLayer(GeoJSONlayer);
- //    }
-
-}
-
-// On créé un calque pour la carte	
-var map = L.map('map');
-L.tileLayer('http://maps.refuges.info/hiking/{z}/{x}/{y}.png', {
-    attribution: '&copy; Contributeurs d\'<a href="http://openstreetmap.org">OpenStreetMap</a>',
-    maxZoom: 18
-}).addTo(map);
-
-var GeoJSONlayer = L.geoJson().addTo(map);
-
-// Fonctions de Géolocalisation, on affiche la zone où on se trouve.
-function onLocationFound(e) {
-    var radius = e.accuracy / 2;
-    L.marker(e.latlng).addTo(map);
-	actualiseGeoJSON();
-}
-function onLocationError(e) {
-	alert(e.message);
-	actualiseGeoJSON();
-}
-function onMove() {
-	// map.removeLayer(GeoJSONlayer);
-	actualiseGeoJSON();
-}
-
-map.locate({setView: true, maxZoom: 14});
-
-// On modifie les données si :
-map.on('locationerror', onLocationError);
-map.on('locationfound', onLocationFound);
-map.on('moveend', onMove); // Si l'utilisateur a fini de trifouiller la position de la carte, on actualise les points
-
-*/
-
-
-
+// Les variables necessaires au bon fonctionnement du truc
 var map;
 var ajaxRequest;
 var plotlist;
 var plotlayers=[];
 
+
+// Fonction lancée dès que le <div> map est chargé
 function initmap(){
-	// set up the AJAX stuff
+	// On teste que le navigateur soit compatible AJAX
 	ajaxRequest=GetXmlHttpObject();
 	if (ajaxRequest==null) {
-		alert ("This browser does not support HTTP Request");
+		alert ("Ce navigateur ne semble pas suporter la technologie AJAX... Vous devrez peut-être le mettre à jour...");
 		return;
 	}
 
-	// set up the map
+	// On créé notre carte vierge
 	map = new L.Map('map');
 
-	var MRIUrl='http://maps.refuges.info/hiking/{z}/{x}/{y}.png';
-	var MRIAttrib='&copy; Contributeurs d\'<a href="http://openstreetmap.org">OpenStreetMap</a>';
-	var MRI = new L.TileLayer(MRIUrl, {minZoom: 1, maxZoom: 12, attribution: MRIAttrib});		
+	var MRIUrl='http://maps.refuges.info/hiking/{z}/{x}/{y}.png'; // Serveur de tuiles MRI
+	var MRIAttrib='&copy; Contributeurs d\'<a href="http://openstreetmap.org">OpenStreetMap</a>'; // Petit texte de crédit en bas à droite
+	var MRI = new L.TileLayer(MRIUrl, {minZoom: 2, maxZoom: 14, attribution: MRIAttrib}); // Création du calque de tuiles nommé MRI
 
-	map.setView(new L.LatLng(45.9, 7.7),9);
-	map.attributionControl.setPrefix('');
-	map.addLayer(MRI);
-	askForPlots();
+	map.setView(new L.LatLng(47, 2),6); // Par défaut on affiche la France entière
+	askForPlots(); // Et on affiche les refuges
+
+	// Fonction lancée quand la géolocalisation a marché
+	function onLocationFound(e) {
+	    L.marker(e.latlng).addTo(map); // On ajoute un marqueur indiquant notre position (bleu par défaut)
+		askForPlots(); // On affiche les refuges dans cette aire
+	}
+	// Fonction lancée quand la géolocalisation a échoué
+	function onLocationError(e) {
+		alert(e.message); // On explique que l'erreur au visiteur
+	}
+	map.locate({watch: true, setView: true, maxZoom: 14}); // On demande de géolocalisé, en continu, avec un zoom de 14 sur notre position
+
+	map.attributionControl.setPrefix(''); // On enlève le crédit vers leaflet
+	map.addLayer(MRI); // On ajoute le calque MRI à la carte
+
+	// Action en cas des évenements suivant
+	map.on('locationerror', onLocationError);
+	map.on('locationfound', onLocationFound);
 	map.on('moveend', onMapMove);
 }
 
+// Fonction lancée quand on veux afficher les points des refuges
 function askForPlots() {
-	// request the marker info with AJAX for the current bounds
+	// On garde en variable les quatres angles de la vue courante (bbox à télécharger)
 	var bounds=map.getBounds();
 	var minll=bounds.getSouthWest();
 	var maxll=bounds.getNorthEast();
+	// On prépare l'adresse à télécharger, avec la bbox.
 	var msg='../exportations/exportations.php?format=geojson&bbox='+minll.lng+','+minll.lat+','+maxll.lng+','+maxll.lat;
-	ajaxRequest.onreadystatechange = stateChanged; 
-	ajaxRequest.open('GET', msg, true);
+
+	// Requete AJAX
+	ajaxRequest.onreadystatechange = stateChanged; // Si on a récupéré le fichier, on apelle stateChanged();
+	ajaxRequest.open('GET', msg, true); //  On apelle le fichier.
 	ajaxRequest.send(null); 
 }
 
-function onMapMove(e) { askForPlots(); }
+function onMapMove(e) { askForPlots(); } // Si on a fini de trifouiller la carte, on rafraichi la liste des refuges
 
+// Fonction appelée pour tester la compatibilité AJAX
 function GetXmlHttpObject() {
 	if (window.XMLHttpRequest) { return new XMLHttpRequest(); }
 	if (window.ActiveXObject)  { return new ActiveXObject("Microsoft.XMLHTTP"); }
 	return null;
 }
 
+// Fonction lancée quand AJAX à retourné quelque chose
 function stateChanged() {
-	// if AJAX returned a list of markers, add them to the map
+	// Si AJAX à bien retourné ce que l'on attendais
 	if (ajaxRequest.readyState==4) {
-		//use the info here that was returned
 		if (ajaxRequest.status==200) {
-			plotlist=eval("(" + ajaxRequest.responseText + ")");
-			removeMarkers();
-			for (i=0;i<plotlist.features.length;i++) {
-				var plotll = new L.LatLng(plotlist.features[i].geometry.coordinates[1],plotlist.features[i].geometry.coordinates[0]);
-				plotll.lat=""+plotll.lat;
-				plotll.lng=""+plotll.lng;
-				var plotmark = new L.Marker(plotll);
-				plotmark.data=plotlist.features[i];
-				map.addLayer(plotmark);
-				plotmark.bindPopup('<a href="' + plotlist.features[i].properties.url + '">' + plotlist.features[i].properties.nom + '</a>');
+			plotlist=eval("(" + ajaxRequest.responseText + ")"); // On enregistre la variable GeoJSON en local
+			removeMarkers(); // On enlève tous les points actuels
+			for (i=0;i<plotlist.features.length;i++) { // Pour chaque point (ligne GeoJSON)
+				var plotll = new L.LatLng(plotlist.features[i].geometry.coordinates[1],plotlist.features[i].geometry.coordinates[0]); // On enregistre ses coordonnées
+				var plotmark = new L.Marker(plotll); // On créé un marqueur (bleu par défaut)
+				map.addLayer(plotmark); // On ajoute un calque pour ce marqueur
+				plotmark.data=plotlist.features[i]; // Useless ?
+				// Une variable contenant le style du marqueur
+				var cabaneIcon = L.icon({
+					iconUrl: '../images/icones/' + plotlist.features[i].properties.type + '.png', // On télécharge la bonne image de marqueur
+					iconSize: [16, 16],
+					iconAnchor: [8, 8],
+					popupAnchor: [0, 0]
+				});
+				plotmark.setIcon(cabaneIcon); // On applique notre icone juste créé
+				plotmark.bindPopup('<a href="' + plotlist.features[i].properties.url + '">' + plotlist.features[i].properties.nom + '</a>'); // On ajoute au marqueur un popup contenant le lien
 				plotlayers.push(plotmark);
 			}
 		}
 	}
 }
 
+// Fonction appelée au avant d'ajouter les marqueurs fournis par AJAX pour enlever les précédents
 function removeMarkers() {
 	for (i=0;i<plotlayers.length;i++) {
 		map.removeLayer(plotlayers[i]);
