4e1c60c7403cc4395ea702d858322510d002d8ea
sletuffe <git@letuffe.org>
2013-03-25 14:34:26 +0100

    J'ai séparé en deux vues, c'est finalement plus lisible que ces gros if qui ne factorise finalement que quelques tags HTML

--- controlleur.php
+++ controlleur.php
@@ -40,6 +40,7 @@ switch ($controlleur->url_decoupee[1])
     case "rss" : case "rss.php" : $vue->type=$controlleur->type="rss"; break;
     case "formulaire_rss.php" : case "formulaire_rss" : $vue->type=$controlleur->type="formulaire_rss"; break;
     case "avis-internaute-commentaire" : $vue->type=$controlleur->type="avis-internaute-commentaire"; break;
+    case "formulaire_exportations" : $vue->type=$controlleur->type="formulaire_exportations"; break;
     default : $vue->type=$controlleur->type="page_introuvable"; break;
 }
 
--- controlleurs/formulaire_exportations.php
+++ controlleurs/formulaire_exportations.php
@@ -5,15 +5,10 @@ ré-utiliser.
 Le traitement proprement dit est dans exportations.php 
 **********************************************************************************************/
 
-require_once ("../includes/config.php");
 require_once ("fonctions_mode_emploi.php");
 require_once ("fonctions_bdd.php");
 require_once ("fonctions_exportations.php");
-require_once ("fonctions_autoconnexion.php");
 require_once ("fonctions_meta_donnees.php");
-
-$vue = new stdclass;
-$vue->etapes = new stdClass; // liste des titres d'aides: licence, baratin ...
 
 $formats="";
 foreach ($config['formats_exportation'] as $formats_possibles)
@@ -27,10 +22,6 @@ if (!isset($_POST['validation'])) // rien de valider, formulaire vierge
 {
     $vue->points = new stdClass; // objet contenant les type de points (en tant quobjets eux memes)
     $vue->massifs = new stdClass;
-
-    $vue->etapes->options = new stdClass ;
-    $vue->etapes->options->titre = "Options d'exportations";
-    $vue->etapes->options->texte = "Veuillez préciser les options des points et des massifs";
 
     // LES TYPES DE POINTS ====================================
     $types_de_point = infos_base()->types_point ;
@@ -82,41 +73,27 @@ else // formulaire validé, affichage du lien et d'un blabla
 {
     $vue->lien_export = new stdClass; // contiendra: URL, description ...
 
-    // ===== BLA BLA
-    $vue->etapes->licence = new stdClass ;
-    $vue->etapes->licence->titre = "Licence";
-    $vue->etapes->licence->texte = "<a href=\"".lien_mode_emploi("licence")."\">Voir les détails sur la licence de refuges.info</a>";
-    
-    $vue->etapes->export = new stdClass ;
-    $vue->etapes->export->titre = "Exportation demandée";
-    $vue->etapes->export->texte = "Voici le lien permanent d'accès direct aux données";
+    $vue->lien_licence = lien_mode_emploi("licence");
 
-	if ($_POST['id_point_type']=="" OR $_POST['id_massif']=="")
+    if ($_POST['id_point_type']=="" OR $_POST['id_massif']=="")
         $vue->lien_export->description="Vous demandez vraiment quelque chose de vide ??";
-	else
-	{
+    else
+    {
         //ya plus de description ?
         //$vue->lien_export->description= $config['formats_exportation'][$_POST['format']]['description'] ;
-	
+    
         $liste_id_point_type = implode(',',$_POST['id_point_type']);
         $liste_id_massif = implode(',',$_POST['id_massif']);
-
-		// limiter à une bbox (si demandé depuis les cartes)
+    
+        // limiter à une bbox (si demandé depuis les cartes)
         if(isset($_POST['sud']) ) 
             $bbox = implode(',',array($_POST['ouest'], $_POST['sud'], $_POST['est'], $_POST['nord']) ) ;
-		
+    
         $options_lien="?limite=sans&amp;format=".$_POST['format']."&amp;liste_id_point_type=$liste_id_point_type&amp;liste_id_massif=$liste_id_massif" . (string) $bbox;
-		
+    
         $vue->lien_export->url = "http://".$config['nom_hote']."/exportations/exportations.php$options_lien";
     } 
+    $vue->type="formulaire_exportations_validation";
 } // fin du else affichage lien
-
-
-// On affiche le tout
-$vue->type = 'exportations/formulaire_exportations';
-$vue->java_lib[]="/vues/formulaire_exportations.js";
-include ($config['chemin_vues']."_entete.html");
-include ($config['chemin_vues']."$vue->type.html");
-include ($config['chemin_vues']."_pied.html");
 
 ?>
