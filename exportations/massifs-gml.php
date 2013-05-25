34561217cd95248a7f6c06a963c0fc9a59f1070f
Dominique92 <protec@cavailhez.fr>
2013-05-25 09:26:08 -0700

    Edition des polygones 2/2

--- exportations/massifs-gml.php
+++ exportations/massifs-gml.php
@@ -9,6 +9,39 @@ require_once ("fonctions_autoconnexion.php");
 require_once ("fonctions_polygones.php");
 require_once ("fonctions_gestion_erreurs.php");
 
+// Si on a des infos remontées, on les mémorise
+if ($data = file_get_contents ('php://input')) { // Récupération du flux en méthode PUT
+    $FeatureCollection = simplexml_load_string (str_replace (array ('gml:', 'feature:'), '', $data));
+    
+    // Polygones
+    foreach ($FeatureCollection as $featureMember)
+    foreach ($featureMember     as $features)
+    foreach ($features          as $attributes)
+    foreach ($attributes        as $geometry)
+    foreach ($geometry          as $polygonMember)
+    foreach ($polygonMember     as $Polygon)
+    foreach ($Polygon           as $outerBoundaryIs)
+    foreach ($outerBoundaryIs   as $LinearRing)
+    foreach ($LinearRing        as $coordinates)
+        $coord [] = (string) $coordinates;
+
+    // LineStrings
+    foreach ($FeatureCollection as $features)
+    foreach ($features          as $attributes)
+    foreach ($attributes        as $geometry)
+    foreach ($geometry          as $LineString)
+    foreach ($LineString        as $coordinates)
+    if ((string) $coordinates)
+        $coord [] = (string) $coordinates;
+
+    //=======================================================================================================
+    //DOMINIQUE: TOTO: C'est là que je requiers l'aide des spacialistes PG pour remonter ce tablo dans la base
+    file_put_contents ('trace.log', var_export ($coord, true));
+    // Chaque ligne du tablo contient la liste des coordonnées d'un polygone
+    //=======================================================================================================
+}
+    
+// Maintenant, on s'occupe du flux descendant
 
 // Générateur de couleurs qui tournent autour du cercle colorimétrique
 $no_coul =   0; // Le numéro de la couleur du massif va s'incrémenter
