655e8da310cd8158492e87205303ac9559aa287a
LeOSW42 <leo-serre@legtux.org>
2014-10-16 17:29:39 +0200

    Ajout d'une newsletter API pour permettre aux utilisateurs de l'API de se tenir informés de l'évolution de l'API et pour nous de lister les utilisateurs, à discuter si vous n'êtes pas Ok

---
+++ api/doc/addMail.php
@@ -0,0 +1,18 @@
+<?php
+
+$list = fopen("mails.txt", "a");
+
+date_default_timezone_set('Europe/Paris');
+
+$string = date('d\/m\/Y H:i').','.$_POST['mail'].",".$_SERVER["REMOTE_ADDR"].";\r\n";
+
+$fwrite = fwrite($list, $string);
+if ($fwrite == false) {
+    echo "Une erreur est survenue lors de l'enregistrement";
+}
+
+fclose ($list);
+
+header('Location: index.html');
+
+?>
