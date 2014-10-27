d1d3a35f33fd4a8c32173bf3231f047d12ff182d
sletuffe <git@letuffe.org>
2014-10-27 17:46:29 +0100

    Règlement de : http://www.refuges.info/forum/viewtopic.php?t=6245 pour pouvoir installer wri dans un sous dossier, pas tester très profondément

--- routage.php
+++ routage.php
@@ -2,11 +2,13 @@
 /*******************************************************************************
 Ceci est un essai actuellement (17/03/2013) de sly pour voir si on pourrait encore mutualiser
 des choses pour le MVC en réduisant tous les appels aux templates, js, styles et autre
-en créant un seul fichier "controlleur.php" qui soit le point d'entrée quasi unique du site
+en créant un seul fichier qui soit le point d'entrée quasi unique du site et qui s'occupe de faire la corresondance :
+1 type URL => 1 controlleur
+
+exemple : http://www.refuges.info/point/X/Y/Z => le controlleur point.php
 
 Il s'occupe alors d'analyser l'url pour en déterminer ce qui doit être fait, appeler les controlleurs
 selon cette url puis ouvrir les vues, toujours selon cet url.
-
 *******************************************************************************/
 require_once ('./includes/config.php');
 require_once ("autoconnexion.php");
@@ -20,9 +22,11 @@ $vue = new stdClass;
 $controlleur->url_complete=$_SERVER['REQUEST_URI'];
 $sans_parametres=explode ('?',$controlleur->url_complete);
 
-// Uniquement /point/5/toto/sfsdf (pas les ?toto=coucou...)
-$controlleur->url_base=$sans_parametres[0];
+// Uniquement /point/5/toto/sfsdf (pas les ?toto=coucou...) et pas le sous dossier dans lequel wri pourrait être installé
+$sous_dossier_installation=str_replace($_SERVER['DOCUMENT_ROOT'],"",dirname(__FILE__));
+$controlleur->url_base=str_replace($sous_dossier_installation,"",$sans_parametres[0]);
 $controlleur->url_decoupee = explode ('/',$controlleur->url_base);
+
 
 // Par défaut, on veut un en-tête et pied de page html, mais dans de rare cas (point-json) on ne veut pas
 // le débat étant à poursuivre ici : http://www.refuges.info/forum/viewtopic.php?t=5294
@@ -30,7 +34,7 @@ $controlleur->avec_entete_et_pied=True;
 
 // On pourrait être tenté de faire une conversion direct de url vers controlleur, mais si un filou commence à indiquer
 // n'importe quelle url, il pourrait réussir à ouvrir des trucs pas souhaités, avec une liste, on s'assure
-// de n'autoriser que ceux que l'on veut
+// de n'autoriser que ceux que l'on a
 switch ($controlleur->url_decoupee[1])
 {
     // sly: Pour toutes ces routes, on est dans un cas simple, l'url correspond au controlleur du même nom, factorisation !
@@ -83,7 +87,11 @@ auto_login_phpbb_users();
 // On appel le controlleur qui pourra, s'il le souhaite, changer le type de vue ($type->vue)
 include ($config['chemin_controlleurs'].$controlleur->type.".php");
 
+// La suite, c'est une somme de "par défaut" sauf si le controlleur à imposer ses choix
+
 // et vérification s'il n'y a pas un commentaire à modérer pour notre équipe de modération
+// FIXME : Dans une logique de rangement parfait, ça ne devrait pas être ici, mais dans chaque contrôleur qui a besoin de modifier le bandeau avec l'étoile, mais la factorisation a eu raison de moi ;-)
+// Si quelqu'un veut le bouger, il a mon feu vert -- sly
 if ($_SESSION['niveau_moderation']>=1)
     $vue->demande_correction=info_demande_correction ();
 
