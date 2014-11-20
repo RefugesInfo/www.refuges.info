cea1e2f95f45fc99ba681bc2796c541df875d933
LeOSW42 <leo-serre@legtux.org>
2014-11-20 14:51:19 +0100

    API bbox, export de points

--- api/controlleur/bbox.php
+++ api/controlleur/bbox.php
@@ -81,9 +81,118 @@ if($req->type_pts != "all") {
     $params->ids_types_point = str_replace($val->type_pts, $val->type_pts_id, $req->type_pts);
 }
 
+$pts_bruts = new stdClass();
+$pts = new stdClass();
+
 $pts_bruts = infos_points($params);
 
+/****************************** INFOS GÉNÉRALES ******************************/
 
-print_r($pts_bruts);
+$i = 0;
+foreach ($pts_bruts as $pt) {
+    $pts->$i = new stdClass();
+    $pts->$i->id = $pt->id_point;
+    $pts->$i->id_gps = $pt->id_point_gps;
+    $pts->$i->nom = $pt->nom;
+    $pts->$i->coord['long'] = $pt->longitude;
+    $pts->$i->coord['lat'] = $pt->latitude;
+    $pts->$i->coord['alt'] = $pt->altitude;
+    $pts->$i->type['id'] = $pt->id_point_type;
+    $pts->$i->type['valeur'] = $pt->nom_type;
+    $pts->$i->places['nom'] = $pt->equivalent_places;
+    $pts->$i->places['valeur'] = $pt->places;
+    $pts->$i->etat['id'] = $pt->conditions_utilisation;
+    $pts->$i->etat['valeur'] = texte_non_ouverte($pt);
+    // On rajoute des informations complémentaires si on demande détaillé
+    if($req->detail == "complet") {
+        $pts->$i->coord['precision']['nom'] = $pt->nom_precision_gps;
+        $pts->$i->coord['precision']['type'] = $pt->id_type_precision_gps;
+        $pts->$i->remarque['nom'] = 'Remarque';
+        $pts->$i->remarque['valeur'] = $pt->remark;
+        $pts->$i->acces['nom'] = 'Accès';
+        $pts->$i->acces['valeur'] = $pt->acces;
+        $pts->$i->proprio['nom'] = $pt->equivalent_proprio;
+        $pts->$i->proprio['valeur'] = $pt->proprio;
+        $pts->$i->createur['id'] = $pt->id_createur;
+        // info sur le créateur de la fiche (authentifié ou non)
+        if ($pt->id_createur==0) // non authentifié
+            $pts->$i->createur['nom']=$pt->nom_createur;
+        else
+            $pts->$i->createur['nom'] = infos_utilisateur($pt->id_createur)->username;
+        $pts->$i->date['derniere_modif'] = $pt->date_derniere_modification;
+        $pts->$i->date['creation'] = $pt->date_creation;
+        $pts->$i->article['demonstratif'] = $pt->article_demonstratif;
+        $pts->$i->article['defini'] = $pt->article_defini;
+        $pts->$i->article['partitif'] = $pt->article_partitif_point_type;
+        $pts->$i->info_comp['site_officiel']['nom'] = $pt->equivalent_site_officiel;
+        $pts->$i->info_comp['site_officiel']['valeur'] = $pt->site_officiel;
+        $pts->$i->info_comp['manque_un_mur']['nom'] = $pt->equivalent_manque_un_mur;
+        $pts->$i->info_comp['manque_un_mur']['valeur'] = $pt->manque_un_mur;
+        $pts->$i->info_comp['cheminee']['nom'] = $pt->equivalent_cheminee;
+        $pts->$i->info_comp['cheminee']['valeur'] = $pt->cheminee;
+        $pts->$i->info_comp['poele']['nom'] = $pt->equivalent_poele;
+        $pts->$i->info_comp['poele']['valeur'] = $pt->poele;
+        $pts->$i->info_comp['couvertures']['nom'] = $pt->equivalent_couvertures;
+        $pts->$i->info_comp['couvertures']['valeur'] = $pt->couvertures;
+        $pts->$i->info_comp['places_matelas']['nom'] = $pt->equivalent_places_matelas;
+        $pts->$i->info_comp['places_matelas']['nb'] = $pt->places_matelas;
+        if($pt->places_matelas == -1)
+            $pts->$i->info_comp['places_matelas']['valeur'] = "Sans";
+        elseif($pt->places_matelas === 0)
+            $pts->$i->info_comp['places_matelas']['valeur'] = "Avec, en nombre inconnu";
+        else
+            $pts->$i->info_comp['places_matelas']['valeur'] = $pt->matelas;
+        $pts->$i->info_comp['latrines']['nom'] = $pt->equivalent_latrines;
+        $pts->$i->info_comp['latrines']['valeur'] = $pt->latrines;
+        $pts->$i->info_comp['bois']['nom'] = $pt->equivalent_bois_a_proximite;
+        $pts->$i->info_comp['bois']['valeur'] = $pt->bois_a_proximite;
+        $pts->$i->info_comp['eau']['nom'] = $pt->equivalent_eau_a_proximite;
+        $pts->$i->info_comp['eau']['valeur'] = $pt->eau_a_proximite;
+    }
+    $i++;
+}
+
+/****************************** FORMATAGE DU TEXTE ******************************/
+
+// On transforme le texte dans la correcte syntaxe
+if($req->format_txt == "texte") {
+    array_walk_recursive($point, 'updatebbcode2txt');
+}
+elseif($req->format_txt == "html") {
+    array_walk_recursive($point, 'updatebbcode2html');
+}
+elseif($req->format_txt == "markdown") {
+    array_walk_recursive($point, 'updatebbcode2markdown');
+}
+array_walk_recursive($point, 'updatebool2char'); // Remplace les False et True en 0 ou 1
+
+/****************************** FORMAT VUE ******************************/
+
+switch ($req->format) {
+    case 'geojson':
+        include('./vue/liste.geojson');
+        break;
+    case 'kmz':
+        include('./vue/liste.kmz');
+        break;
+    case 'kml':
+        include('./vue/liste.kml');
+        break;
+    case 'gml':
+        include('./vue/liste.gml');
+        break;
+    case 'gpx':
+        include('./vue/liste.gpx');
+        break;
+    case 'gpi':
+        include('./vue/liste.gpi');
+        break;
+    case 'csv':
+        include('./vue/liste.csv');
+        break;
+    default:
+        include('./vue/liste.geojson');
+        break;
+}
 
 ?>
