088b751fb482b14119f6c2cbe8b4c404c99268ec
LeOSW42 <leo-serre@legtux.org>
2014-11-22 14:36:18 +0100

    API - Export en GPI

--- controlleurs/api.points.controleur.php
+++ controlleurs/api.points.controleur.php
@@ -34,7 +34,7 @@ $req->type_points = $_GET['type_points'];
 
 // Ici c'est les valeurs possibles
 $val = new stdClass();
-$val->format = array("geojson", "kmz", "kml", "gml", "gpx",/* "gpi", "csv",*/ "xml"/*, "yaml", "rss"*/);
+$val->format = array("geojson", "kmz", "kml", "gml", "gpx", "gpi",/* "csv",*/ "xml"/*, "yaml", "rss"*/);
 $val->format_texte = array("bbcode", "texte", "markdown", "html");
 $val->detail = array("simple", "complet");
 $val->type_points = array("cabane", "refuge", "gite", "pt_eau", "sommet", "pt_passage", "bivouac", "lac");
@@ -348,12 +348,9 @@ switch ($req->format) {
     case 'gml':
         include('../vues/api/points.vue.gml');
         break;
-    case 'gpx':
+    case 'gpx': case 'gpi':
         include('../vues/api/points.vue.gpx');
         break;
-    // case 'gpi':
-    //     include('../vues/api/points.vue.gpi');
-    //     break;
     // case 'csv':
     //     include('../vues/api/points.vue.csv');
     //     break;
