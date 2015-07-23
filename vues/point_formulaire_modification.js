0abb10a0ff46c2b0bb76833db8dccfa80a4f8ba8
Dominique92 <protec@cavailhez.fr>
2015-07-23 10:02:20 +0200

    Retour en format decimal pour saisie de coordonnées

--- vues/point_formulaire_modification.js
+++ vues/point_formulaire_modification.js
@@ -49,6 +49,7 @@ window.addEventListener('load', function() {
 
 			curseur = new L.Marker.Position( // Le pointeur à déplacer
 				new L.LatLng(<?=$vue->point->latitude?>, <?=$vue->point->longitude?>), {
+					projectionType: 'decimal',
 					prefixe: 'curseur-',
 					draggable: true,
 					riseOnHover: true, // The marker will get on top of others when you hover the mouse over it.
