e166c50a99cbc4950fbb1b9c5bd569a8ce3ba844
Dominique92 <protec@cavailhez.fr>
2015-10-29 09:05:48 +0100

    Censure des messages de réservation ==> Retour arrière avant les modifs

--- vues/style.css.php
+++ vues/style.css.php
@@ -335,14 +335,6 @@ A.infobulle:hover SPAN { /* qd on passe dessus, ca affiche */
     z-index: 500000;
 }
 
-/*=========CENSURE==========*/
-.erreur_saisie {
-	border: 2px double red;
-	background-color: yellow;
-	padding: 10px;
-	margin-bottom: 10px;
-}
-
 /*=========PUBLICITE==========*/
 @media screen and (max-width: 940px) {
   .publicite {
