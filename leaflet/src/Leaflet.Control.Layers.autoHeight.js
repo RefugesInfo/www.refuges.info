4e849dbac300eeb4ccb2a89bd29c8c57c0cb8560
Dominique Cavailhez <protec@cavailhez.fr>
2016-03-13 17:12:19 +0100

    Ajout Leaflet.Control.Layers.autoHeight (barre scroll dans le layerSwitcher)

---
+++ leaflet/src/Leaflet.Control.Layers.autoHeight.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016 Dominique Cavailhez
+ * Tune the control layer height accordighly with map width
+ * in case of the layer list exceed the map height
+ */
+
+L.Control.Layers.autoHeight = L.Control.Layers.extend({
+
+	onAdd: function(map) {
+		L.Control.Layers.prototype.onAdd.call(this, map);
+
+		map.on('resize', this._autoHeight, this);
+		this._autoHeight();
+
+		return this._container;
+	},
+
+	_autoHeight: function() {
+		this._baseLayersList.style.maxHeight = (this._map._container.offsetHeight - 86) + 'px';
+		this._baseLayersList.style.overflow = 'auto';
+	}
+});
