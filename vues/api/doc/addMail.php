a4ce43c9d4522fcbbecf7a3b307fe3277ca644f3
sletuffe <git@letuffe.org>
2018-03-11 09:07:33 +0100

    ça écrit un fichier à la racine, c'est crade : à réparer vraiment ou à supprimer définitivement

--- vues/api/doc/addMail.php
+++ vues/api/doc/addMail.php
@@ -3,18 +3,18 @@
  * FIXME: A refaire au format MVC, là, c'est non fonctionnel vue que j'ai sauvegardé ça de coté dans le dossier des vues
  */
 
-$list = fopen("mails.txt", "a");
+//$list = fopen("mails.txt", "a");
 
 date_default_timezone_set('Europe/Paris');
 
-$string = date('d\/m\/Y H:i').','.$_POST['mail'].",".$_SERVER["REMOTE_ADDR"].";\r\n";
+//$string = date('d\/m\/Y H:i').','.$_POST['mail'].",".$_SERVER["REMOTE_ADDR"].";\r\n";
 
-$fwrite = fwrite($list, $string);
-if ($fwrite == false) {
+//$fwrite = fwrite($list, $string);
+//if ($fwrite == false) {
     echo "Une erreur est survenue lors de l'enregistrement";
-}
+//}
 
-fclose ($list);
+//fclose ($list);
 
 header('Location: index.html');
 
