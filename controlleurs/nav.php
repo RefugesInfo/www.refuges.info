f3245fea3ae5f8e7e1fee27709a1aaddeba15213
sletuffe <git@letuffe.org>
2019-05-23 12:04:02 +0200

    Je ré-ordonne le titre de la page pour que l'info pertinente (quel massif ?) soit mise en premier

--- controlleurs/nav.php
+++ controlleurs/nav.php
@@ -44,7 +44,8 @@ if ($id_polygone)
   $polygone=infos_polygone ($id_polygone);
   if (!$polygone->erreur) 
   {
-      $vue->titre="Cartes des refuges, sommets et sources/points d'eau dans $polygone->art_def_poly $polygone->type_polygone $polygone->article_partitif $polygone->nom_polygone";
+      $vue->titre=ucfirst($polygone->type_polygone)." $polygone->article_partitif $polygone->nom_polygone : Refuges, cabanes, sommets et points d'eau sur une carte";
+      $vue->intro="Voici les refuges, cabanes, sommets et points d'eau dans $polygone->art_def_poly $polygone->type_polygone $polygone->article_partitif $polygone->nom_polygone";
   }
   else
     $vue->titre="Polygone demandé incorrect : $polygone->message";
