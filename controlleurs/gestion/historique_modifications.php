337acca2e127c845c66f56e478e4b87eea1a8624
sletuffe <git@letuffe.org>
2019-09-09 03:56:03 +0200

    Viewer d'historique de modifications un peu plus lisible et ne débordant pas de la page (faudrait faire un peu de style pour que ça soit joli)

--- controlleurs/gestion/historique_modifications.php
+++ controlleurs/gestion/historique_modifications.php
@@ -8,19 +8,22 @@ si ça semble utile, je pourrais l'améliorer avec conditions sur le point et p'
 require_once ('mise_en_forme_texte.php');
 require_once ('utilisateur.php');
 
-$query_log_modification="select avant,apres,id_user,date_modification::timestamp(0) as date_modification from historique_modifications_points order by date_modification desc LIMIT 100";
+$query_log_modification="select *,date_modification::timestamp(0) as date from historique_modifications_points order by date_modification desc LIMIT 100";
 if (! ($res = $pdo->query($query_log_modification)))
     return erreur("Requête en erreur, impossible d'afficher l'historique de modifications",$query_log_modification);
         
-  //Constuisons maintenant la liste des points demandés avec toutes les informations sur chacun d'eux
   while ($modification_point = $res->fetch())
   {
     if ($modification_point->id_user!=0)
         $utilisateur=infos_utilisateur($modification_point->id_user);
     else
         $utilisateur->username="anonyme ??";
+    $point_avant=unserialize($modification_point->avant);
+    $point_apres=unserialize($modification_point->apres);
     
     $modification_point->moderateur=$utilisateur->username;
+    $modification_point->point_avant=$point_avant;
+    $modification_point->point_apres=$point_apres;
     $vue->modifications_points[]=$modification_point;
   }
 //d($vue);
