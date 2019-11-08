95da2c9c6b471448aacc1ff8e273a96e7cec6e07
Dominique Cavailhez <Dominique92@dc9.fr>
2019-11-08 18:33:13 +0100

    Upgrade forum phpBB-3.2.8_FR...

---
+++ forum/config.php
@@ -0,0 +1,25 @@
+<?php
+// phpBB 3.2.x auto-generated configuration file
+// Do not change anything in this file!
+
+// Récupère les données locales
+require(__DIR__.'/../includes/config.php');
+require(__DIR__.'/../config_privee.php');
+
+$dbms = 'phpbb\\db\\driver\\postgres';
+$dbhost = $config_wri['serveur_pgsql'];
+$dbport = '';
+$dbname = $config_wri['base_pgsql'];
+$dbuser = $config_wri['utilisateur_pgsql'];
+$dbpasswd = $config_wri['mot_de_passe_pgsql'];
+$table_prefix = 'phpbb3_';
+$phpbb_adm_relative_path = 'adm/';
+$acm_type = 'phpbb\\cache\\driver\\file';
+
+@define('PHPBB_INSTALLED', true);
+
+if (!@$config_wri['debug'])
+	@define('PHPBB_ENVIRONMENT', 'production');
+
+// @define('PHPBB_DISPLAY_LOAD_TIME', true);
+// @define('DEBUG_CONTAINER', true);
