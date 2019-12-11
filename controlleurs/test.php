4a7b99063045d4517e30cfd96466017fcdc975bd
sletuffe <git@letuffe.org>
2019-12-11 13:30:10 +0100

    Fichier de test mis à jour avec plus d'exemples (en commentaires) et plus d'includes

--- controlleurs/test.php
+++ controlleurs/test.php
@@ -8,31 +8,39 @@ http://dev.refuges.info/test
 
 **********************************************************************************************/
 // On est pas là pour les perfs, alors on inclus tout pour être tranquille !
+require_once ("config.php");
 require_once ("bdd.php");
 require_once ("commentaire.php");
 require_once ("point.php");
 require_once ("utilisateur.php");
 require_once ("polygone.php");
-require_once ("mise_en_forme_texte.php");
 require_once ("gestion_erreur.php");
 require_once ("meta_donnee.php");
 require_once ("xml.class.php");
 require_once ("api.php");
 require_once ("nouvelle.php");
+require_once ("mise_en_forme_texte.php");
+require_once ("upload_max_filesize.php");
+require_once ("zipfile.php");
+
+
 session_start();
+
 //auto_login_phpbb_users();
 
-print("prouf");
+//print("prouf");
 //$_SESSION['toto']="coucou";
-print_r($_SESSION);
-print_r($_GET);
-die();
-$html=bbcode2html($texte,$autoriser_html=FALSE,$autoriser_balise_img=TRUE);
+//print_r($_SESSION);
+//print_r($_GET);
+//die();
+//$html=bbcode2html($texte,$autoriser_html=FALSE,$autoriser_balise_img=TRUE);
 
-$point = infos_point(105);
+//$point = infos_point(105);
 
-d(lien_point($point,true));
+// d ( ) et la fonction de debug qui print les variables passée et une trace des appels
+//d(lien_point($point,true));
 //d($config_wri['sous_dossier_installation']);
 
-exit();
-?>
+//print_r($_SERVER);
+
+
