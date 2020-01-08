5538062623852e6ceb54d1b9aecb5cdb0301b857
Dominique Cavailhez <Dominique92@dc9.fr>
2020-01-08 11:23:14 +0100

    Correction bug nombre de places Correction bug /gps

--- vues/includes/cartes.js
+++ vues/includes/cartes.js
@@ -1,15 +1,18 @@
 <?php // Contient les déclarations communes aux cartes
 ?>
 const layersSwitcher = controlLayersSwitcher({
-	init: '<?=$config_wri["carte_base"]?>',
+	<?if (isset($config_wri["carte_base"])){?>
+		init: '<?=$config_wri["carte_base"]?>',
+	<?}?>
 	baseLayers: {
 		'Refuges.info': layerOsmMri(),
 		'OpenTopo': layerOsmOpenTopo(),
 		'Outdoors': layerThunderforest('<?=$config_wri['thunderforest_key']?>', 'outdoors'),
 		'OSM-fr': layerOsm('//{a-c}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png'),
-		<?php if ($vue->type != 'point_formulaire_modification' && $vue->type != 'edit') { ?>
-		'IGN': layerIGN('<?=$config_wri['ign_key']?>', 'GEOGRAPHICALGRIDSYSTEMS.MAPS'),
-		'IGN Express': layerIGN('<?=$config_wri['ign_key']?>', 'GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.CLASSIQUE'),
+		<?php if (!isset($vue) || // For /gps
+			($vue->type != 'point_formulaire_modification' && $vue->type != 'edit')) { ?>
+			'IGN': layerIGN('<?=$config_wri['ign_key']?>', 'GEOGRAPHICALGRIDSYSTEMS.MAPS'),
+			'IGN Express': layerIGN('<?=$config_wri['ign_key']?>', 'GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.CLASSIQUE'),
 		<?php } ?>
 		'SwissTopo': layerSwissTopo('ch.swisstopo.pixelkarte-farbe'),
 		'Autriche': layerKompass('KOMPASS Touristik'),
