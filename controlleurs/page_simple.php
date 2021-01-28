03614287e7be35014e3ae83bd2dcb43dec2c221e
sletuffe <git@letuffe.org>
2021-01-28 22:34:54 +0100

    Oulla, j'ai repéré un bug ! celui qui connait l'url peut modifier tous les commentaires anonymes du site à cause de l'opérateur == plutôt que === Je corrige et en plus je corrige un retour de code http érronné 403 vs 200 + cadeau, on peut mettre une raison pour laquelle on est refusé

--- controlleurs/page_simple.php
+++ controlleurs/page_simple.php
@@ -5,11 +5,11 @@
 // sly: Il reste possible de toute façon de générer un 404 avec une page sur mesure
 switch ($vue->http_status_code) {
 	case 403:
-		$vue->contenu = $vue->titre = "Erreur 403 - Vous n'avez pas les droits d'accès à la page \"$controlleur->url_base\"";
+		$vue->contenu = $vue->titre = "Erreur 403 - Vous n'avez pas les droits d'accès à la page ".$controlleur->url_base." ".$vue->interdit_raison;
 		break;
 
 	case 404:
-		$vue->contenu = $vue->titre = "Erreur 404 - La page demandée \"$controlleur->url_base\" est introuvable sur refuges.info";
+		$vue->contenu = $vue->titre = "Erreur 404 - La page demandée ".$controlleur->url_base." est introuvable sur refuges.info";
 
 }
 
--- routes/gestion.routes.php
+++ routes/gestion.routes.php
@@ -13,22 +13,32 @@
 **************************************************/
 
 // Par défaut
-$vue->http_status_code = 403;
 $controlleur->type = 'page_simple';
 
 // C'est le point unique qui contrôle les autorisations de toutes les URL /gestion...
 switch ($controlleur->url_decoupee[1]) {
 	case 'moderation':
 		$commentaire = infos_commentaire($_REQUEST['id_commentaire'],true);
-		if ($commentaire->id_createur_commentaire == $_SESSION['id_utilisateur'])
+		if ($commentaire->id_createur_commentaire === $_SESSION['id_utilisateur'] OR $_SESSION['niveau_moderation'])
 			$controlleur->type = 'gestion/moderation';
+        else
+        {
+          $vue->http_status_code = 403;
+          $vue->interdit_raison =", pour modifier un commentaire, soit ça doit être le votre, soit vous devez être modérateur global, êtes vous bien connecté ?";
+        }
+    break;
 
 	case 'modifier_modeles':
 	case 'commentaires_attente_correction':
 	case 'historique_modifications':
 		if ($_SESSION['niveau_moderation'])
 			$controlleur->type = 'gestion/'.$controlleur->url_decoupee[1];
-		break;
+        else
+        {
+          $vue->http_status_code = 403;        
+          $vue->interdit_raison = ", vous devez être un modérateur du site, pour accéder à cette page, êtes vous bien connecté ?";
+        }
+    break;
 
 	default:
 		$vue->http_status_code = 404;
