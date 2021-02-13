fe5eb5ca59209b8c317b8156ddbaf59b6cc1d7ae
sletuffe <git@letuffe.org>
2021-02-13 04:40:13 +0100

    En voyant cette vue dur à lire, je n'ai pas pu m'empêcher de la nettoyer

---
+++ vues/api/polygones.vue.json.php
@@ -0,0 +1,40 @@
+<?php
+
+$secondes_de_cache = 60;
+$ts = gmdate("D, d M Y H:i:s", time() + $secondes_de_cache) . " GMT";
+header("Content-disposition: filename=polygones.json");
+header("Content-Type: application/json; UTF-8"); // rajout du charset
+header("Content-Transfer-Encoding: binary");
+header("Pragma: cache");
+header("Expires: $ts");
+if($config_wri['autoriser_CORS']===TRUE) header("Access-Control-Allow-Origin: *");
+header("Cache-Control: max-age=$secondes_de_cache");
+?>
+{
+  "type": "FeatureCollection",
+  "generator": "Refuges.info API",
+  "copyright": "<?=$config_wri['copyright_API']?>",
+  "timestamp": "<?=date(DATE_ATOM)?>",
+  "features": 
+  [
+<?php
+$j = 0;
+foreach ($polygones as $polygone) 
+{
+  $j++;
+?>
+    {
+      "type": "Feature",
+      "id": <?=$polygone->id?>,
+      "geometry": <?=$polygone->geometrie?>,
+      "properties":
+      <?php unset ($polygone->geometrie); ?>
+      <?=json_encode($polygone)?>
+      
+    }
+<?php
+	if ($j != $nombre_polygones) echo ",";
+}
+?>
+  ]
+}
