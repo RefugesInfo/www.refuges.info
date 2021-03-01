55d68bd6733aea2be8293f51ddfd440fdc23ec48
Dominique Cavailhez <Dominique92@dc9.fr>
2021-03-01 21:51:50 +0100

    Simplification des noms d'icônes

--- images/icones/index.php
+++ images/icones/index.php
@@ -1,122 +1,161 @@
 <?php
 /* Cet utilitaire produit toutes les icônes SVG utilisées par les cartes
-
-Les définitions sont dans un tableau $config_wri['definition_icones']
-situé dans includes/config.php comportant :
+/images/icones/debut-du-nom-<code>_<code>_<code>.svg ou .png
+Chaque code désigne une forme élémentaire :
 
 Icônes
 ------
-'icone' => 'eau' point d'eau)
-'icone' => 'lac'
-'icone' => 'precaire' (manque un mur)
-'icone' => 'sommet'
-'icone' => 'triangle' (passage délicat)
-Défaut : cabane, forme de bâtiment
+Les 2 première lettres comptent : i<une minuscule>[n'importe quelles minuscules]
+	ie[au] (point d'eau)
+	il[ac]
+	is[ommet]
+	it[riangle] (passage délicat)
+	Défaut : cabane, forme de bâtiment
 
 Couleurs (des formes de bâtiment)
 --------------------------------
-couleur = 'black' : batiment blanc, toit et murs noirs
-couleur = 'green' : batiment vert (gîte)
-couleur = 'red' : batiment rouge (refuge gardé)
-couleur = '...' : autres couleurs CSS
-Défaut : cabane ocre
+c suivi d'une couleur CSS
+	cblack (batiment blanc, toit et murs noirs)
+	cgreen (batiment vert, gîte)
+	cred (batiment rouge, refuge gardé)
+	c... (autres couleurs CSS)
+	Défaut : cabane ocre et toit rouge
 
 Attributs
 ---------
-'cle'
-'croix' (une grande croix noire)
-'eau' (petite goute d'eau
-'feu' (cheminée et fumée)
-'texte' => 'X' (caractère X)
+Seule la première lettre compte
+	e[au] (petite goute d'eau)
+	f'feu' (cheminée et fumée)
+	k[ey] (clé)
+	p[recaire] (manque un mur)
+	x(une grande croix noire)
+
+Caractère au centre
+-------------------
+	a123 le caractère ascii 123 (en décimal)
 
 */
 
-// On récupère les definitions
-// et on les met en forme pour pouvoir les traiter facilement
-include_once ('../../includes/config.php');
-$def = @$config_wri['definition_icones'] [$_GET ['nom']] ?: [];
-$att = array_filter ($def, 'is_int', ARRAY_FILTER_USE_KEY);
+//-------------------------------------
+// Tableau de correspondance temporaire
+// A retirer quand tous les noms des icones auront été codés
+$alias = [
+	'ancien-point-d-eau' => 'ieau_x',
+	'batiment-en-montagne' => 'cblack_a63',
+	'batiment-inutilisable' => 'cblack_x',
+	'cabane-avec-eau' => 'eau',
+	'cabane-avec-moyen-de-chauffage' => 'feu',
+	'cabane-avec-moyen-de-chauffage-et-eau-a-proximite' => 'eau_feu', // Peut être résumé par 'e_f'
+	'cabane-cle' => 'key',
+	'cabane-eau-a-proximite' => 'eau',
+	'cabane-manque-un-mur' => 'precaire',
+	'cabane-non-gardee' => 'icabane', // Le vrai code est '' mais on peut mettre icabane, ça fait plus joli
+	'cabane-sans-places-dormir' => 'a48',
+	'gite-d-etape' => 'cblue', // Car la forme de bâtiment est par défaut
+	'inutilisable' => 'cblack_x',
+	'lac' => 'ilac',
+	'passage-delicat' => 'itriangle_a33', // Peut être résumé par 'it_a33'
+	'point-d-eau' => 'ieau',
+	'refuge-garde' => 'cred',
+	'sommet' => 'isommet', // Peut être résumé par 'is'
+];
+if (isset ($alias[$_GET['nom']]))
+	$_GET['nom'] = $alias[$_GET['nom']];
+
+
+//------------------------------------------------------
+// On va rechercher les arguments dans le nom du fichier
+preg_match_all ('/([a-z])([a-z0-9]*)_/', $_GET['nom'].'_', $matches);
+foreach ($matches[1] AS $k=>$v)
+	$args[$v] = $matches[2][$k];
 
 // Arguments spéciaux
-$taille = @$def['taille'] ?: 24;
-$couleur_toit = @$def['couleur'] ?: 'red';
-$couleur_mur = @$def['couleur'] ?: '#e08020';
-$couleur = @$def['couleur'] ?: '#ffeedd';
-if ($couleur == 'black')
+$taille = @$args['t'] ?: 24;
+$icone = @$args['i'][0];
+$couleur_toit = @$args['c'] ?: 'red';
+$couleur_mur = @$args['c'] ?: '#e08020';
+$couleur = @$args['c'] ?: '#ffeedd';
+if (isset($args['p'])) // Pas de murs extérieurs aux bâtiments précaires
+	$couleur_mur = '#ffeedd';
+if ($couleur == 'black') // Murs et toit noir, murs blancs
 	$couleur = 'white';
+
 
 //------------------------------------------------
 // On commence à capturer la sortie du template
 ob_start();
 
 // Template SVG
-?><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="<?=$taille?>" width="<?=$taille?>">
+?><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" height="<?=$taille?>" width="<?=$taille?>">
 <?php
 
-if (@$def['icone'] == 'eau') {
-?>	<ellipse cx="8" cy="10" rx="4.5" ry="4.5" stroke-width="0.5" stroke="#005e5e" fill="cyan" />
-	<ellipse cx="8" cy="10" rx="3" ry="3" stroke-width="0" fill="#005e5e" />
-	<ellipse cx="8.7" cy="9.2" rx="3" ry="3" stroke-width="0" fill="cyan" />
-	<path d="M4.2 7.6 l3.8 -6.6,3.8 6.6" stroke-width="0.5" stroke="#005e5e" fill="cyan" />
+if ($icone == 'e') { /* Eau */
+?>	<ellipse cx="12" cy="15" rx="6.75" ry="6.75" stroke-width="0.75" stroke="#005e5e" fill="cyan" />
+	<ellipse cx="12" cy="15" rx="4.5" ry="4.5" stroke-width="0" fill="#005e5e" />
+	<ellipse cx="13" cy="14" rx="4.5" ry="4.5" stroke-width="0" fill="cyan" />
+	<path d="M6.3 11.4 l5.7 -10,5.7 10" stroke-width="0.75" stroke="#005e5e" fill="cyan" />
 
-<?php } elseif (@$def['icone'] == 'lac') {
-?>	<ellipse cx="5" cy="6" rx="4" ry="3" stroke="#204A87" fill="#204A87" />
-	<ellipse cx="9" cy="9" rx="6" ry="3" stroke="#204A87" fill="#204A87" />
-	<ellipse cx="7.5" cy="8" rx="6" ry="3" stroke="#204A87" fill="#204A87" />
+<?php } elseif ($icone == 'l') { /* Lac */
+?>	<ellipse cx="7.5" cy="9" rx="6" ry="4.5" stroke="#204A87" fill="#204A87" />
+	<ellipse cx="13.5" cy="13.5" rx="9" ry="4.5" stroke="#204A87" fill="#204A87" />
+	<ellipse cx="11" cy="12" rx="9" ry="4.5" stroke="#204A87" fill="#204A87" />
+	
+<?php } elseif ($icone == 's') { /* Sommet */
+?>	<path d="M0 26 l8 -17,3 5,5 -12,8 24" stroke="white" fill="#583E24" />
 
-<?php } elseif (@$def['icone'] == 'precaire') /* Manque un mur */ {
-?>	<path d="M1 8.2 l7 -7,7 7" stroke-width="2" stroke-linecap="round" stroke="red" fill="none" />
-	<path d="M4 8 l8 0" stroke-linecap="round" stroke="red" />
-	<path d="M3 11.5 l10 0" stroke-linecap="round" stroke="red" />
-	<path d="M3 15 l10 0" stroke-linecap="round" stroke="red" />
-
-<?php } elseif (@$def['icone'] == 'sommet') {
-?>	<path d="M0 17.3 l5 -11,2 3,3 -8,6 16" stroke="white" fill="#583E24" />
-
-<?php } elseif (@$def['icone'] == 'triangle') {
-?>	<path d="M1.2 15.25 l6.88 -11.75,6.8 11.75 Z" stroke-width="1.5" stroke="red" fill="white" />
+<?php } elseif ($icone == 't') { /* Triangle (passage délicat) */
+?>	<path d="M1.75 23 l10.3 -18,10.2 18 Z" stroke-width="2" stroke="red" fill="white" />
 
 <?php } else { /* Bâtiments */
-?>	<path d="M2 7 l0 8.75,12 0,0 -8.75" stroke-width="0.5" stroke="<?=$couleur_mur?>" fill="<?=$couleur?>" />
-	<path d="M1 8.2 l7 -7,7 7" stroke-width="2" stroke-linecap="round" stroke="<?=$couleur_toit?>" fill="<?=$couleur?>" />
-	<?php if (!isset ($def['couleur']) && /* Porte */
-		!isset ($def['texte']) &&
-		!in_array('eau', $att) &&
-		!in_array('cle', $att)) {
-	?>	<rect x="6" y="9" width="4" height="7" stroke="none" fill="#e08020" />
+?>	<path d="M3 10.7 l0 13,18 0,0 -13" stroke-width="0.5" stroke="<?=$couleur_mur?>" fill="<?=$couleur?>" />
+	<path d="M1.5 12.3 l10.5 -10.5,10.5 10.5" stroke-width="3" stroke-linecap="round" stroke="<?=$couleur_toit?>" fill="<?=$couleur?>" />
+	<?php if (!isset ($args['couleur']) && /* Porte */
+		!isset ($args['a']) &&
+		!isset ($args['c']) &&
+		!isset ($args['e']) &&
+		!isset ($args['p'])) {
+	?>	<rect x="9" y="13.5" width="6" height="10" stroke="none" fill="#e08020" />
 
 	<?php }
 	}
 
-// Attirubuts
-if (in_array('cle', $att)) {
-?>	<ellipse cx="13.2" cy="5.2" rx="2" ry="2" stroke-width="1.6" stroke="black" fill="none" />
-	<path d="M12 6 l-9 9,-1.5 -1.5,2.8 -0.3,0.1 -2.7,1.9 1.8" stroke-width="1.4" stroke="black" fill="none" />
+// Attributs
+if (isset ($args['a'])) { /* Ascii */
+?>	<text x="<?=$args['a']==33?9:7.5?>" y="21" font-size="18px" >&#<?=$args['a']?>;</text>
 
-<?php } if (in_array('croix', $att)) {
-?>	<path d="M0.75 2.5 l14.5 12.75" stroke-width="1.5" stroke-linecap="round" stroke="black" fill="none" />
-	<path d="M0.75 15.25 l14.5 -12.75" stroke-width="1.5" stroke-linecap="round" stroke="black" fill="none" />
+<?php } if (isset($args['e'])) { /* Eau */
+?>	<ellipse cx="16.5" cy="19.2" rx="3.4" ry="3.4" stroke-width="0.75" stroke="#005e5e" fill="cyan" />
+	<path d="M13.65 17.36 l2.85 -5,2.85 5" stroke-width="0.75" stroke="#005e5e" fill="cyan" />
 
-<?php } if (in_array('eau', $att)) {
-?>	<ellipse cx="11" cy="12.75" rx="2.25" ry="2.25" stroke-width="0.5" stroke="#005e5e" fill="cyan" />
-	<path d="M9.1 11.55 l1.9 -3.3,1.9 3.3" stroke-width="0.5" stroke="#005e5e" fill="cyan" />
+<?php } if (isset($args['f'])) { /* Feu */
+?>	<rect x="3" y="2" width="3" height="7" fill="black" />
+	<ellipse cx="8" cy="2.5" rx="1.5" ry="2" stroke="#444444" fill="#888888" />
+	<ellipse cx="12.5" cy="3.5" rx="2" ry="3" stroke="#444444" fill="#888888" />
+	<ellipse cx="19.5" cy="5.5" rx="4" ry="5" stroke="#444444" fill="#888888" />
 
-<?php } if (in_array('feu', $att)) {
-?>	<rect x="2" y="1.5" width="2" height="5" fill="black" />
-	<ellipse cx="5.5" cy="2" rx="1" ry="1" stroke="#444444" fill="#888888" />
-	<ellipse cx="9" cy="2" rx="1.5" ry="1.5" stroke="#444444" fill="#888888" />
-	<ellipse cx="13.5" cy="2.5" rx="2" ry="2" stroke="#444444" fill="#888888" />
+<?php } if (isset($args['k'])) { /* Clé */
+?>	<ellipse cx="19.8" cy="7.8" rx="3" ry="3" stroke-width="2.4" stroke="black" fill="none" />
+	<path d="M18 9 l-13.5 13.5,-2.25 -2.25,4.2 -0.45,0.15 -4,3 2.7" stroke-width="2.1" stroke="black" fill="none" />
 
-<?php } if (isset ($def['texte'])) {
-?>	<text x="<?=$def['texte']=='!'?6:5?>" y="14.5" font-size="12px" ><?=$def['texte']?></text>
+<?php } if (isset($args['p'])) { /* Précaire (manque un mur) */
+?>	<path d="M6 12 l12 0" stroke-width="1.5" stroke-linecap="round" stroke="red" />
+	<path d="M4.5 17.25 l15 0" stroke-width="1.5" stroke-linecap="round" stroke="red" />
+	<path d="M4.5 22.5 l15 0" stroke-width="1.5" stroke-linecap="round" stroke="red" />
+
+
+<?php } if (isset($args['x'])) { /* Croix (barré) */
+?>	<path d="M1 1 l22 22" stroke-width="2" stroke-linecap="round" stroke="black" fill="none" />
+	<path d="M1 23 l22 -22" stroke-width="2" stroke-linecap="round" stroke="black" fill="none" />
 
 <?php } ?></svg>
 <?php
+
 
 //--------------------------------------------
 // On fini de capturer la sortie du template
 $svg =  ob_get_contents();
 ob_end_clean();
+
 
 //---------------------
 // Sortie en format SVG
@@ -128,8 +167,9 @@ if ($_GET['ext'] == 'svg') {
 	echo $svg;
 }
 
+
 //---------------------
-// Sortie en format SVG
+// Sortie en format PNG
 if ($_GET['ext'] == 'png') {
 	header ('Content-type: image/png');
 	header ('Cache-Control: max-age=86000');
--- ressources/exemples-icones.php
+++ ressources/exemples-icones.php
@@ -6,18 +6,165 @@ body {
 </style>
 
 <h1>Exemples d'icônes SVG et PNG générées en PHP</h1>
-<?php
-
-include_once ('../includes/config.php');
-
-foreach (array_keys ($config_wri['definition_icones']) AS $nom) { ?>
-	<p>
-		<img src="../images/icones/<?=$nom?>.svg" />
-		<a href="../images/icones/<?=$nom?>.svg">
-			/images/icones/<?=$nom?>.svg</a>
-		&nbsp; &nbsp;
-		<img src="../images/icones/<?=$nom?>.png" />
-		<a href="../images/icones/<?=$nom?>.png">
-			/images/icones/<?=$nom?>.png</a>
-	</p>
-<?php } ?>
+<p>
+	<img src="../images/icones/ancien-point-d-eau.svg" />
+	<a href="../images/icones/ancien-point-d-eau.svg">
+		/images/icones/ancien-point-d-eau.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/ancien-point-d-eau.png" />
+	<a href="../images/icones/ancien-point-d-eau.png">
+		/images/icones/ancien-point-d-eau.png</a>
+</p>
+<p>
+	<img src="../images/icones/batiment-en-montagne.svg" />
+	<a href="../images/icones/batiment-en-montagne.svg">
+		/images/icones/batiment-en-montagne.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/batiment-en-montagne.png" />
+	<a href="../images/icones/batiment-en-montagne.png">
+		/images/icones/batiment-en-montagne.png</a>
+</p>
+<p>
+	<img src="../images/icones/batiment-inutilisable.svg" />
+	<a href="../images/icones/batiment-inutilisable.svg">
+		/images/icones/batiment-inutilisable.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/batiment-inutilisable.png" />
+	<a href="../images/icones/batiment-inutilisable.png">
+		/images/icones/batiment-inutilisable.png</a>
+</p>
+<p>
+	<img src="../images/icones/cabane-avec-eau.svg" />
+	<a href="../images/icones/cabane-avec-eau.svg">
+		/images/icones/cabane-avec-eau.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/cabane-avec-eau.png" />
+	<a href="../images/icones/cabane-avec-eau.png">
+		/images/icones/cabane-avec-eau.png</a>
+</p>
+<p>
+	<img src="../images/icones/cabane-avec-moyen-de-chauffage.svg" />
+	<a href="../images/icones/cabane-avec-moyen-de-chauffage.svg">
+		/images/icones/cabane-avec-moyen-de-chauffage.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/cabane-avec-moyen-de-chauffage.png" />
+	<a href="../images/icones/cabane-avec-moyen-de-chauffage.png">
+		/images/icones/cabane-avec-moyen-de-chauffage.png</a>
+</p>
+<p>
+	<img src="../images/icones/cabane-avec-moyen-de-chauffage-et-eau-a-proximite.svg" />
+	<a href="../images/icones/cabane-avec-moyen-de-chauffage-et-eau-a-proximite.svg">
+		/images/icones/cabane-avec-moyen-de-chauffage-et-eau-a-proximite.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/cabane-avec-moyen-de-chauffage-et-eau-a-proximite.png" />
+	<a href="../images/icones/cabane-avec-moyen-de-chauffage-et-eau-a-proximite.png">
+		/images/icones/cabane-avec-moyen-de-chauffage-et-eau-a-proximite.png</a>
+</p>
+<p>
+	<img src="../images/icones/cabane-cle.svg" />
+	<a href="../images/icones/cabane-cle.svg">
+		/images/icones/cabane-cle.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/cabane-cle.png" />
+	<a href="../images/icones/cabane-cle.png">
+		/images/icones/cabane-cle.png</a>
+</p>
+<p>
+	<img src="../images/icones/cabane-eau-a-proximite.svg" />
+	<a href="../images/icones/cabane-eau-a-proximite.svg">
+		/images/icones/cabane-eau-a-proximite.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/cabane-eau-a-proximite.png" />
+	<a href="../images/icones/cabane-eau-a-proximite.png">
+		/images/icones/cabane-eau-a-proximite.png</a>
+</p>
+<p>
+	<img src="../images/icones/cabane-manque-un-mur.svg" />
+	<a href="../images/icones/cabane-manque-un-mur.svg">
+		/images/icones/cabane-manque-un-mur.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/cabane-manque-un-mur.png" />
+	<a href="../images/icones/cabane-manque-un-mur.png">
+		/images/icones/cabane-manque-un-mur.png</a>
+</p>
+<p>
+	<img src="../images/icones/cabane-non-gardee.svg" />
+	<a href="../images/icones/cabane-non-gardee.svg">
+		/images/icones/cabane-non-gardee.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/cabane-non-gardee.png" />
+	<a href="../images/icones/cabane-non-gardee.png">
+		/images/icones/cabane-non-gardee.png</a>
+</p>
+<p>
+	<img src="../images/icones/cabane-sans-places-dormir.svg" />
+	<a href="../images/icones/cabane-sans-places-dormir.svg">
+		/images/icones/cabane-sans-places-dormir.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/cabane-sans-places-dormir.png" />
+	<a href="../images/icones/cabane-sans-places-dormir.png">
+		/images/icones/cabane-sans-places-dormir.png</a>
+</p>
+<p>
+	<img src="../images/icones/gite-d-etape.svg" />
+	<a href="../images/icones/gite-d-etape.svg">
+		/images/icones/gite-d-etape.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/gite-d-etape.png" />
+	<a href="../images/icones/gite-d-etape.png">
+		/images/icones/gite-d-etape.png</a>
+</p>
+<p>
+	<img src="../images/icones/inutilisable.svg" />
+	<a href="../images/icones/inutilisable.svg">
+		/images/icones/inutilisable.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/inutilisable.png" />
+	<a href="../images/icones/inutilisable.png">
+		/images/icones/inutilisable.png</a>
+</p>
+<p>
+	<img src="../images/icones/lac.svg" />
+	<a href="../images/icones/lac.svg">
+		/images/icones/lac.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/lac.png" />
+	<a href="../images/icones/lac.png">
+		/images/icones/lac.png</a>
+</p>
+<p>
+	<img src="../images/icones/passage-delicat.svg" />
+	<a href="../images/icones/passage-delicat.svg">
+		/images/icones/passage-delicat.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/passage-delicat.png" />
+	<a href="../images/icones/passage-delicat.png">
+		/images/icones/passage-delicat.png</a>
+</p>
+<p>
+	<img src="../images/icones/point-d-eau.svg" />
+	<a href="../images/icones/point-d-eau.svg">
+		/images/icones/point-d-eau.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/point-d-eau.png" />
+	<a href="../images/icones/point-d-eau.png">
+		/images/icones/point-d-eau.png</a>
+</p>
+<p>
+	<img src="../images/icones/refuge-garde.svg" />
+	<a href="../images/icones/refuge-garde.svg">
+		/images/icones/refuge-garde.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/refuge-garde.png" />
+	<a href="../images/icones/refuge-garde.png">
+		/images/icones/refuge-garde.png</a>
+</p>
+<p>
+	<img src="../images/icones/sommet.svg" />
+	<a href="../images/icones/sommet.svg">
+		/images/icones/sommet.svg</a>
+	&nbsp; &nbsp;
+	<img src="../images/icones/sommet.png" />
+	<a href="../images/icones/sommet.png">
+		/images/icones/sommet.png</a>
+</p>
