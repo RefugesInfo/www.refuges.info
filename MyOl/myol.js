94395852f5b8491aa0a1082a410b1e20aa01b86e
Dominique Cavailhez <Dominique92@dc9.fr>
2021-04-14 17:24:04 +0200

    Petit bug dans la couche chemineur

--- MyOl/myol.js
+++ MyOl/myol.js
@@ -963,7 +963,7 @@ function layerPyreneesRefuges(options) {
  */
 function layerChemineur(options) {
 	return layerVectorURL(Object.assign({
-		baseUrl: '//chemineur.fr/ext/Dominique92/GeoBB/gis.php?poi=',
+		baseUrl: '//chemineur.fr/ext/Dominique92/GeoBB/gis.php?cat=',
 		urlSuffix: '3,8,16,20,23,30,40,44,58,64',
 		strategy: ol.loadingstrategy.bboxLimit,
 		receiveProperties: function(properties) {
