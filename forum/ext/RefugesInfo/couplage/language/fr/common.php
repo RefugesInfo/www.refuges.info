161c1340b6b0e70d48b39f23793f60d853a37ec5
sletuffe <git@letuffe.org>
2021-05-18 10:13:26 +0200

    Problème avec gmail rare, un peu de militantisme anti-live.com et le dossier chez Live s'appel Indésirables

--- forum/ext/RefugesInfo/couplage/language/fr/common.php
+++ forum/ext/RefugesInfo/couplage/language/fr/common.php
@@ -39,6 +39,6 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'EMAIL_REMIND' => 'Adresse courriel associée à votre compte. Si vous ne l’avez pas modifiée via votre panneau d’utilisateur, il s’agit de l’adresse que vous avez fournie lors de votre enregistrement.<br/><br/>'.
-	'<span style="color:red">Si votre email termine en @gmail.com ou @hotmail.com/fr ou @live.fr/com ou @outlook.com (ou autre chez microsoft) ou chez yahoo.fr/.com, des problèmes sont parfois rencontrés avec ces fournisseurs. Il faut vérifier dans le répertoire SPAMS et/ou être patient et attendre des fois plusieurs heures avant de recevoir le mail qui vous rappel votre mot de passe. Si cela vous est possible, utilisez une autre adresse email</span><br/>'.
+	'<span style="color:red">Si votre email termine en @gmail.com ou @hotmail.com/fr ou @live.fr/com ou @outlook.com (ou autre chez microsoft) ou chez yahoo.fr/.com, des problèmes sont parfois rencontrés avec ces fournisseurs (surtout Microsoft/Live). Il faut vérifier dans le répertoire indésirables et/ou être patient et attendre des fois plusieurs heures avant de recevoir le mail qui vous rappel votre mot de passe. Si cela vous est possible, utilisez une autre adresse email</span><br/>'.
 	'Si le problème persiste, n\'hesitez pas à nous contacter via le <a href="../../posting.php?mode=post&f=2">formulaire de contact</a>',
 ));
