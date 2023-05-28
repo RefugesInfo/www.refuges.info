c2909a088102c46e63616176324f7ac49451c326
sletuffe <git@letuffe.org>
2023-05-28 18:29:53 +0200

    Il manquait l'option pour tester les liens externes, et je n'exclus plus nav qui est une source pour récupérer les pages des fiches

--- ressources/outils/detection-lien-invalides/recuperation-liste-csv.sh
+++ ressources/outils/detection-lien-invalides/recuperation-liste-csv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-linkchecker -t 4 -o csv --ignore="nav" --ignore="avis_internaute_commentaire" --ignore="point_ajout_commentaire" --ignore-url="forum" http://www.refuges.info
+linkchecker -t 4 -o csv --check-extern --ignore="avis_internaute_commentaire" --ignore="point_ajout_commentaire" --ignore-url="forum" https://www.refuges.info
