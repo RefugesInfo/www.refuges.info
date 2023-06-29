cf9f121c258f8d760e165142b7a4a88d2abf51af
sletuffe <git@letuffe.org>
2023-06-29 14:53:47 +0200

    Quelques erreurs html, pas de titre, simplification d'un truc qui ne marchait pas

--- controlleurs/avis_internaute_commentaire.php
+++ controlleurs/avis_internaute_commentaire.php
@@ -11,41 +11,44 @@ $commentaire=infos_commentaire($controlleur->url_decoupee[1]);
 
 if (!empty($commentaire->erreur))
 {
-    $vue->http_status_code = 404;
-    $vue->type = "page_simple";
-    $vue->titre=$commentaire->message;
+  $vue->http_status_code = 404;
+  $vue->type = "page_simple";
+  $vue->titre=$commentaire->message;
 }
 else
 {
-    $vue->commentaire=$commentaire;
-    $vue->commentaire->texte_affichage=bbcode2html($commentaire->texte,FALSE,FALSE);
+  $vue->commentaire=$commentaire;
+  $vue->commentaire->texte_affichage=bbcode2html($commentaire->texte,FALSE,FALSE);
 
-    $vue->commentaire->lien=lien_point($commentaire,True);
-    
-    /**************************** l'action  ******************************/
-    if (!empty($_REQUEST['valider']))
+  $vue->commentaire->lien=lien_point($commentaire,True);
+  
+  /**************************** l'action  ******************************/
+  if (!empty($_REQUEST['valider']))
+  {
+    $vue->type="page_simple";
+    // Si l'internaute est connecté au forum ou qu'il a saisi la lettre anti-robot
+    if (est_connecte() or $_REQUEST['anti_robot']=="f")
     {
-        $vue->type="page_simple";
-        // Si l'internaute est connecté au forum ou qu'il a saisi la lettre anti-robot
-        if (est_connecte() or $_REQUEST['anti_robot']=="f")
-        {
-            $commentaire->demande_correction=$_REQUEST['demande_correction'];
-            $commentaire->raison_demande_correction=$_REQUEST['raison_demande_correction'];
-            modification_ajout_commentaire($commentaire);
-            $vue->contenu="Merci pour votre aide au tri, ";
-        }
-        else
-            $vue->contenu="Oups ? la lettre anti_robot saisie n'est pas la bonne, utilisez le bouton \"Retour de votre navigateur pour reprendre la saisie\", ou ";
-        
-        $vue->lien=$vue->commentaire->lien;
-        $vue->contenu.="vous pouvez retourner sur : ";
-        $vue->titre_lien="la fiche de $commentaire->nom";
+      $commentaire->demande_correction=$_REQUEST['demande_correction'];
+      $commentaire->raison_demande_correction=$_REQUEST['raison_demande_correction'];
+      modification_ajout_commentaire($commentaire);
+      $vue->contenu="Merci pour votre aide au tri, ";
     }
     else
-    {
-        $vue->lien_que_mettre=lien_wiki("que_mettre");
-        if (!est_connecte())
-            $vue->test_anti_robot=True;
-    }
+      $vue->contenu="Oups ? la lettre anti_robot saisie n'est pas la bonne, utilisez le bouton \"Retour de votre navigateur pour reprendre la saisie\", ou ";
+    
+    $vue->lien=$vue->commentaire->lien;
+    $vue->contenu.="vous pouvez retourner sur : ";
+    $vue->titre_lien="la fiche de $commentaire->nom";
+    $vue->titre="Classement commentaire sur $vue->titre_lien";
+  }
+  else
+  {
+    $vue->lien_que_mettre=lien_wiki("que_mettre");
+    if (!est_connecte())
+      $vue->test_anti_robot=True;
+    $vue->titre="Classement commentaire sur la fiche de $commentaire->nom";
+
+  }
 }
 
