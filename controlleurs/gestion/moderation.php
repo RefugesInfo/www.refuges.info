1f36df5431c7807ee84f2d90373cdb05fc0b0fde
sletuffe <git@letuffe.org>
2024-03-10 20:47:01 +0100

    Bug : https://www.refuges.info/forum/viewtopic.php?t=13473 j'avais oublié que empty() est true quand  vaut 0 or 0 c'est l'id de l'anonyme, donc ça ne pouvait plus changer d'un anonyme à un user

--- controlleurs/gestion/moderation.php
+++ controlleurs/gestion/moderation.php
@@ -10,39 +10,37 @@ require_once ('mise_en_forme_texte.php');
 // Traitement des actions
 switch ($_REQUEST['type'])
 {
-	case 'transfert_autre_point':
-		$commentaire->id_point=$_REQUEST['id_autre_point'];
-        $autre_point=infos_point($_REQUEST['id_autre_point']);
-	case 'modification':
-	case 'transfert_forum':
-	case 'suppression_photo':
+  case 'transfert_autre_point':
+    $commentaire->id_point=$_REQUEST['id_autre_point'];
+    $autre_point=infos_point($_REQUEST['id_autre_point']);
+  case 'modification':
+  case 'transfert_forum':
+  case 'suppression_photo':
 
-		$commentaire->texte=stripslashes($_REQUEST['texte']);
-		$commentaire->auteur_commentaire=stripslashes($_REQUEST['auteur_commentaire']);
-		if (!empty($commentaire->id_createur_commentaire))
-          $commentaire->id_createur_commentaire=stripslashes($_REQUEST['id_createur']);
-          
-		$commentaire->rotation=$_REQUEST['rotation'];
-		
-		// On suppose qu'après modification par un modérateur il a fait les corrections nécessaires et qu'il n'est plus besoin de lister ce commentaire dans les demandes de modération.
-		// le but est de gagner du temps aux modérataurs qui n'ont pas besoin d'aller retirer manuellement de la liste des tâches à faire.
-		if (est_moderateur())
-          $commentaire->demande_correction=0;
-          
-        // On applique toutes les modifications, la fonction s'occupant de retourner une éventuelle erreur et un message en testant presque tous les cas possible (point inexistant, commentaire vide, ...)  
-		$vue->retour=modification_ajout_commentaire($commentaire);
-		
-		if ($_REQUEST['type'] == 'transfert_forum') 		// ensuite on le transfert sur le forum (si cela échoue, un message d'erreur est retourné)
-          $vue->retour=transfert_forum($commentaire);
-                
-		if ($_REQUEST['type'] == 'suppression_photo') 		// et on supprime la photo
-          $vue->retour=suppression_photos($commentaire);
-        break;
-     
-
-	case 'suppression':
-		$vue->retour=suppression_commentaire($commentaire);
-		break; 
+    $commentaire->texte=stripslashes($_REQUEST['texte']);
+    $commentaire->auteur_commentaire=stripslashes($_REQUEST['auteur_commentaire']);
+    $commentaire->id_createur_commentaire=stripslashes($_REQUEST['id_createur_commentaire']);
+      
+    $commentaire->rotation=$_REQUEST['rotation'];
+    
+    // On suppose qu'après modification par un modérateur il a fait les corrections nécessaires et qu'il n'est plus besoin de lister ce commentaire dans les demandes de modération.
+    // le but est de gagner du temps aux modérataurs qui n'ont pas besoin d'aller retirer manuellement de la liste des tâches à faire.
+    if (est_moderateur())
+      $commentaire->demande_correction=0;
+      
+    // On applique toutes les modifications, la fonction s'occupant de retourner une éventuelle erreur et un message en testant presque tous les cas possible (point inexistant, commentaire vide, ...)  
+    $vue->retour=modification_ajout_commentaire($commentaire);
+    
+    if ($_REQUEST['type'] == 'transfert_forum') 		// ensuite on le transfert sur le forum (si cela échoue, un message d'erreur est retourné)
+      $vue->retour=transfert_forum($commentaire);
+            
+    if ($_REQUEST['type'] == 'suppression_photo') 		// et on supprime la photo
+      $vue->retour=suppression_photos($commentaire);
+    break;
+    
+  case 'suppression':
+    $vue->retour=suppression_commentaire($commentaire);
+    break; 
 }
 
 
