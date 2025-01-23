4820abbd389c2399502e7001d389da09f3cd6d82
Dominique Cavailhez <dominique@c92.fr>
2025-01-23 17:04:11 +0100

    Trace la raison du rejet d'une requête 3/3

--- controlleurs/gestion/historique_traces.php
+++ controlleurs/gestion/historique_traces.php
@@ -3,8 +3,10 @@ $numero = $controlleur->url_decoupee[3] ?: 0;
 $where_list = [
 	'accepte' => ' WHERE ext_error IS NULL AND mode != \'Rejeté\'',
 	'rejete' => ' WHERE ext_error IS NOT NULL OR mode = \'Rejeté\'',
-	'cleantalk' => ' WHERE ext_error LIKE \'%CleanTalk%\'',
-	'blockbotposts' => ' WHERE ext_error LIKE \'%BlockBotPosts%\' and ext_error NOT LIKE \'%CleanTalk%\'',
+	'cleantalk' => ' WHERE ext_error NOT LIKE \'%security reasons%\' AND ext_error LIKE \'%CleanTalk%\'',
+	'blockbotposts' => ' WHERE ext_error LIKE \'%security reasons%\' AND ext_error NOT LIKE \'%CleanTalk%\'',
+	'les2' => ' WHERE ext_error LIKE \'%security reasons%\' OR ext_error LIKE \'%CleanTalk%\'',
+	'nini' => ' WHERE ext_error IS NOT NULL AND ext_error NOT LIKE \'%security reasons%\' AND ext_error NOT LIKE \'%CleanTalk%\'',
 	'topic' => ' WHERE topic_id = '.$numero,
 	'post' => ' WHERE post_id = '.$numero,
 	'point' => ' WHERE point_id = '.$numero,
