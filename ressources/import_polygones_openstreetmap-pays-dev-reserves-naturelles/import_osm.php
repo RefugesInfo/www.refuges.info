6fcc2bc9c3ed0be25920f7ffdef2eb613025729c
sletuffe <sylvain@letuffe.org>
2025-01-25 23:04:30 +0100

    Je rajoute des if (false) pour choisir plus en détail ce qu'on veut activer ou non dans la procédure

--- ressources/import_polygones_openstreetmap-pays-dev-reserves-naturelles/import_osm.php
+++ ressources/import_polygones_openstreetmap-pays-dev-reserves-naturelles/import_osm.php
@@ -75,99 +75,111 @@ $params="-d ".$config_wri['base_pgsql']." -H ".$config_wri['serveur_pgsql']." -U
 putenv("PGPASSWORD=".$config_wri['mot_de_passe_pgsql']);
 
 // L'index et l'id_polygone_type dans notre base, les éléments d'après sont les critères OSM qui correspondent à nos types de polygones
-//$conditions[12][]="<has-kv k=\"boundary\" v=\"national_park\"/>"; // parc nationnaux
-//$conditions[12][]="<has-kv k=\"boundary\" v=\"protected_area\"/>"; // zone de protections variées
-//$conditions[12][]="<has-kv k=\"leisure\" v=\"nature_reserve\"/>"; // réserves naturelles
-//$conditions_sql[12]="(
-//                        boundary='national_park' 
-//                        or (boundary='protected_area' and (protect_class in ('1','2','4') or protect_id in ('1','2','4')))
-//                        or (leisure='nature_reserve')
-//                        )";
-//$conditions[10][]="<has-kv k=\"boundary\" v=\"administrative\"/><has-kv k=\"admin_level\" v=\"6\"/>"; // département ou équivalent
-//$conditions_sql[10]="(boundary='administrative' and admin_level='6')";
-$conditions[17][]="<has-kv k=\"boundary\" v=\"administrative\"/><has-kv k=\"admin_level\" v=\"4\"/>"; // région ou équivalent
-$conditions_sql[17]="(boundary='administrative' and admin_level='4')";
-//$conditions[6][]="<has-kv k=\"boundary\" v=\"administrative\"/><has-kv k=\"admin_level\" v=\"2\"/>"; // Pays
-//$conditions_sql[6]="(boundary='administrative' and admin_level='2')";
+if (false) // mettre à true si on veut les zones protégées
+{
+$conditions[12][]="<has-kv k=\"boundary\" v=\"national_park\"/>"; // parc nationnaux
+$conditions[12][]="<has-kv k=\"boundary\" v=\"protected_area\"/>"; // zone de protections variées
+$conditions[12][]="<has-kv k=\"leisure\" v=\"nature_reserve\"/>"; // réserves naturelles
+$conditions_sql[12]="(
+                        boundary='national_park' 
+                        or (boundary='protected_area' and (protect_class in ('1','2','4') or protect_id in ('1','2','4')))
+                        or (leisure='nature_reserve')
+                        )";
+}
 
+if (false) // mettre à true si on veut les départements
+{
+  $conditions[10][]="<has-kv k=\"boundary\" v=\"administrative\"/><has-kv k=\"admin_level\" v=\"6\"/>"; // département ou équivalent
+  $conditions_sql[10]="(boundary='administrative' and admin_level='6')";
+}
+if (false) // mettre à true si on veut les régions
+{
+  $conditions[17][]="<has-kv k=\"boundary\" v=\"administrative\"/><has-kv k=\"admin_level\" v=\"4\"/>"; // région ou équivalent
+  $conditions_sql[17]="(boundary='administrative' and admin_level='4')";
+}
+if (false) // mettre à true si on veut les pays
+{
+  $conditions[6][]="<has-kv k=\"boundary\" v=\"administrative\"/><has-kv k=\"admin_level\" v=\"2\"/>"; // Pays
+  $conditions_sql[6]="(boundary='administrative' and admin_level='2')";
+}
 
 foreach ($conditions as $id_polygone_type => $criteres_osm)
 {
   // Si on commente, ça fait tout de chez tout, mais ça peut être long
   // selon le type que l'on veut importer (des fois, tout n'est pas à refaire) on peut laisser ce if
-  if ($id_polygone_type == 17 ) 
+  // suppression des zones protégées de notre base
+  if (false) //******** ICI on supprime tous ce qu'on a déjà du type indiqué
   {
-    // suppression des zones protégées de notre base
     $pdo->query("DELETE FROM polygones WHERE id_polygone_type=$id_polygone_type;");
-    foreach ($criteres_osm as $condition)
+  }
+  foreach ($criteres_osm as $condition)
+  {
+    foreach ($bboxes as $bbox)
     {
-      foreach ($bboxes as $bbox)
-      {
-      $a_remplacer=array("##sud##" ,"##nord##" ,
-      "##est##" ,"##ouest##" ,
-      "##conditions_osm##");
-      $remplacement=array($bbox->sud, $bbox->nord,
-                          $bbox->est, $bbox->ouest,
-                          $condition);
-                          
-      $xml_query=str_replace($a_remplacer,$remplacement,$overpass_query_template);
-      print($xml_query);
-      print("Téléchargement de $condition sur $bbox->nom_polygone...\n");
-      
-      if (false) //******** ICI on déclenche l'appel OverPass
-      {
-        $result=requete_overpass_api($xml_query);
-        file_put_contents("$dossier_temporaire/tmp.osm",$result);
-      }
-      
-      
-      $command="osm2pgsql -c $params -C 100 -s -S ./default.style -p $prefix -G -l $dossier_temporaire/tmp.osm";
-      print ($command);
-      
-      if (false) //******** ICI on déclenche l'import osm2pgsql
-      {
-        passthru($command);
-      }
-      
-      if (false) //******** ICI on efface le fichier téléchargé
-      {
-        unlink("$dossier_temporaire/tmp.osm");
-      }
-      // copy de celles importées par osm2pgsql
-      
-      if (false) //******** ICI on insère tous les polygones du id_type_polygone choisi
-      {
-        $query="INSERT INTO polygones 
-                          (id_polygone_type,message_information_polygone,url_exterieure,site_web,nom_polygone,source,geom) 
-                            SELECT $id_polygone_type,CONCAT(\"description:restrictions\",\"description:restrictions2\"),\"description:restrictions3\"),\"website:regulation\",website,name,'openstreetmap et contributeurs',ST_Multi(way) 
-                          FROM ".$prefix."_polygon 
-                          where 
-                          name is not null
-                          and
-                          $conditions_sql[$id_polygone_type]
-                          ;";
-        $pdo->query($query) or die($query);
-      }
-      }
-      reset($bboxes);
+    $a_remplacer=array("##sud##" ,"##nord##" ,
+    "##est##" ,"##ouest##" ,
+    "##conditions_osm##");
+    $remplacement=array($bbox->sud, $bbox->nord,
+                        $bbox->est, $bbox->ouest,
+                        $condition);
+                        
+    $xml_query=str_replace($a_remplacer,$remplacement,$overpass_query_template);
+    print($xml_query);
+    print("Téléchargement de $condition sur $bbox->nom_polygone...\n");
+    
+    if (false) //******** ICI on déclenche l'appel OverPass
+    {
+      $result=requete_overpass_api($xml_query);
+      file_put_contents("$dossier_temporaire/tmp.osm",$result);
     }
-    // Traitements post import des doublons que notre procédure créé en trop (liée à la superposition de nos zones)
-    if (false) //******** ICI on enlève des doublons ? 
+    
+    
+    $command="osm2pgsql -c $params -C 100 -s -S ./default.style -p $prefix -G -l $dossier_temporaire/tmp.osm";
+    print ($command);
+    
+    if (false) //******** ICI on déclenche l'import osm2pgsql
     {
-      $query="delete from polygones 
-                  where 
-                    id_polygone_type=$id_polygone_type 
-                  and 
-                    id_polygone not in 
-                      (SELECT min(id_polygone) 
-                      FROM 
-                        polygones 
-                      where 
-                        id_polygone_type=$id_polygone_type 
-                      GROUP BY nom_polygone
-                      );";
-        $pdo->query($query) or die($query);
-     }
+      passthru($command);
+    }
+    
+    if (false) //******** ICI on efface le fichier téléchargé
+    {
+      unlink("$dossier_temporaire/tmp.osm");
+    }
+    // copy de celles importées par osm2pgsql
+    
+    if (false) //******** ICI on insère tous les polygones du id_type_polygone choisi
+    {
+      $query="INSERT INTO polygones 
+                        (id_polygone_type,message_information_polygone,url_exterieure,site_web,nom_polygone,source,geom) 
+                          SELECT $id_polygone_type,CONCAT(\"description:restrictions\",\"description:restrictions2\",\"description:restrictions3\"),\"website:regulation\",website,name,'openstreetmap et contributeurs',ST_Multi(way) 
+                        FROM ".$prefix."_polygon 
+                        where 
+                        name is not null
+                        and
+                        $conditions_sql[$id_polygone_type]
+                        ;";
+      $pdo->query($query) or die($query);
+    }
+    }
+    reset($bboxes);
+  }
+  // Traitements post import des doublons que notre procédure créé en trop (liée à la superposition de nos zones)
+  if (false) //******** ICI on enlève des doublons ? 
+  {
+    $query="delete from polygones 
+                where 
+                  id_polygone_type=$id_polygone_type 
+                and 
+                  id_polygone not in 
+                    (SELECT min(id_polygone) 
+                    FROM 
+                      polygones 
+                    where 
+                      id_polygone_type=$id_polygone_type 
+                    GROUP BY nom_polygone
+                    );";
+    $pdo->query($query) or die($query);
   }
 }
 // suppression des tables "temporaires" de osm2pgsql qui reste du tout dernier import
