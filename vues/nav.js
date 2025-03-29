65bf01d48de044112e934231b17a8eaff1bc0017
sletuffe <sylvain@letuffe.org>
2025-03-29 16:47:40 +0100

    Passage à php 8.2 : un warning sur variable not set

--- vues/nav.js
+++ vues/nav.js
@@ -2,9 +2,11 @@ var map = mapNav({
   target: 'carte-nav',
   host: '<?=$config_wri["sous_dossier_installation"]?>',
   mapKeys: <?=json_encode($config_wri['mapKeys'])?>,
-  id_polygone: <?=isset($vue->polygone)?$vue->polygone->id_polygone:0?>,
-  id_polygone_type: <?=isset($vue->polygone)?$vue->polygone->id_polygone_type:0?>,
+  <?php if (isset($vue->polygone)) { // si on a un polygone on zoom sur lui ?>
+  id_polygone: <?=$vue->polygone->id_polygone?>,
+  id_polygone_type: <?=$vue->polygone->id_polygone_type?>,
   extent: <?=json_encode($vue->polygone->extent)?>,
+  <?php } ?>
   initSelect: true,
 });
 
