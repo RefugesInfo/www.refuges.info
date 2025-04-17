8d047a929341f8b972bb71e571e3ada5d54d01d2
sletuffe <sylvain@letuffe.org>
2025-04-17 22:48:53 +0200

    Si on demandait plus de nouvelles, j'aoubliais de reporter les conditions de filtres sur les polygones

--- controlleurs/nouvelles.php
+++ controlleurs/nouvelles.php
@@ -13,6 +13,8 @@ else
     $nombre = $config_wri['defaut_nombre_nouvelles_page_nouvelles'];
 
 $vue->types_nouvelles = $_GET ['quoi'] ??  'points,forums,commentaires';
+if (!empty($_GET ['ids_polygones']))
+  $vue->ids_polygones = "&amp;ids_polygones=" . $_GET ['ids_polygones'];
 
 // Pour une raison qui m'échappe, le robot google appel cette page avec nombre=56200 puis 56300 puis 56400, quelle qu'en soit la raison, soyons raisonnable, aucune raison d'appeler cette page avec autant, ça va rendre une page html gigantesque !
 if ($nombre > 2000 )
