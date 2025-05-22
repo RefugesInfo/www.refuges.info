01c9af745327df39bc3f56a43b42f5f4984d0309
sletuffe <sylvain@letuffe.org>
2025-05-22 21:40:56 +0200

    Aller, on complète, on peut maintenant sur la page des nouvelles et du flux rss sélectionner les grottes, les points d'eau, les sommets, et tout ça avec un menu de sélection

--- includes/config.php
+++ includes/config.php
@@ -139,13 +139,14 @@ $config_wri['id_gite_etape']=9;
 $config_wri['id_refuge_garde']=10; 
 $config_wri['id_point_d_eau']=23;
 $config_wri['id_batiment_en_montagne']=28;
+$config_wri['id_grotte']=29;
 
 // Catégorie "tout type de refuges" ( ce sont les ids des refuges gardés, non gardés, gîtes)
 // certes une gestion par catégorie directement dans la base serait préférable, mais en 2024 on a 1 seules catégorie donc, bon...
 $config_wri['tout_type_refuge']=array($config_wri['id_cabane_non_gardee'],$config_wri['id_gite_etape'],$config_wri['id_refuge_garde']);
 
 // Catégorie "tout type d'abris, pareil qu'avant, les grottes en plus
-$config_wri['tout_type_d_abri']=array_merge($config_wri['tout_type_refuge'],array(29));
+$config_wri['tout_type_d_abri']=array_merge($config_wri['tout_type_refuge'],array($config_wri['id_grotte']));
 
 //là aussi ça parait crétin de stocker ça en dur, alors qu'il y a bien une technique pour lister dynamiquement le nom des champs, et ben, allez savoir pourquoi, chez postgres, cette méthode bouffe ~10ms !! Vu que je m'en sers plusieurs fois en plus, quitte à en arriver là, je l'écris ici et zou
 //pensez à ajouter vous même à la main "geom" si vous voulez la géométrie, car c'est justement là le but de ne pas mettre "*" : éviter de récupérer la géométrie pour rien
